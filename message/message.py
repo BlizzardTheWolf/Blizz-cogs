9091ca2e8343a8e873d77958cdce5219b77d5f70
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-09-13 11:22:04 +0200

    Create message.py

---
+++ message/message.py
@@ -0,0 +1,41 @@
+from redbot.core import commands
+import discord
+
+class MessageCog(commands.Cog):
+    """Moderator DM commands."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @commands.has_permissions(manage_messages=True)
+    async def message(self, ctx, embed: bool = True, *, message_content):
+        """
+        Send a message to a user via DM through the bot.
+        Usage: [p]message [embed=True] <message>
+        """
+        try:
+            # Remove the invoking user's name from the message content
+            message_content = message_content.replace(ctx.author.mention, '').strip()
+
+            # Find the mentioned user (you can also use user IDs)
+            if ctx.message.mentions:
+                target_user = ctx.message.mentions[0]
+            else:
+                await ctx.send("You need to mention a user to send a message to.")
+                return
+
+            if embed:
+                # If embed=True, send the message as an embed
+                embed = discord.Embed(description=message_content, color=discord.Color.green())
+                await target_user.send(embed=embed)
+            else:
+                # If embed=False, send the message as plain text
+                await target_user.send(message_content)
+
+            await ctx.send(f"Message sent to {target_user.mention} successfully!")
+        except Exception as e:
+            await ctx.send(f"An error occurred: {e}")
+
+def setup(bot):
+    bot.add_cog(MessageCog(bot))
