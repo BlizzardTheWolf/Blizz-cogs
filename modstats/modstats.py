06c7eb6b6b9c9f725926111e761ed98285b4196d
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-09-13 17:32:02 +0200

    Update modstats.py

--- modstats/modstats.py
+++ modstats/modstats.py
@@ -1,33 +1,59 @@
 import discord
-from redbot.core import commands, Config
+from redbot.core import commands
 from datetime import datetime, timedelta
-import os  # Import the os module
-import json  # Import the json module
+import os
+import json
 
 class ModeratorStatsCog(commands.Cog):
     """Moderation statistics tracking."""
 
     def __init__(self, bot):
         self.bot = bot
-        self.config = Config.get_conf(self, identifier=1234567890)  # Change identifier
-
-        # Define the directory path for storing JSON files
         self.data_dir = "/home/panda/.local/share/Red-DiscordBot/data/lake/modstats"
 
-        default_guild_settings = {
-            "mutes": [],
-            "bans": [],
-            "kicks": [],
-            "warns": []
-        }
+    @commands.command()
+    @commands.has_guild_permissions(manage_messages=True)
+    async def mute(self, ctx, user: discord.User, duration: str, *, reason: str):
+        """Mute a user for a specified duration."""
+        # Your mute logic here
 
-        self.config.register_guild(**default_guild_settings)
+        await self.log_action(ctx.guild, "mutes")
+
+        await ctx.send(f"{user.mention} has been muted for {duration}.")
 
     @commands.command()
-    @commands.guild_only()
-    @commands.has_permissions(manage_messages=True)
+    @commands.has_guild_permissions(ban_members=True)
+    async def ban(self, ctx, user: discord.User, *, reason: str):
+        """Ban a user."""
+        # Your ban logic here
+
+        await self.log_action(ctx.guild, "bans")
+
+        await ctx.send(f"{user.mention} has been banned.")
+
+    @commands.command()
+    @commands.has_guild_permissions(kick_members=True)
+    async def kick(self, ctx, user: discord.User, *, reason: str):
+        """Kick a user."""
+        # Your kick logic here
+
+        await self.log_action(ctx.guild, "kicks")
+
+        await ctx.send(f"{user.mention} has been kicked.")
+
+    @commands.command()
+    @commands.has_guild_permissions(manage_messages=True)
+    async def warn(self, ctx, user: discord.User, *, reason: str):
+        """Warn a user."""
+        # Your warn logic here
+
+        await self.log_action(ctx.guild, "warns")
+
+        await ctx.send(f"{user.mention} has been warned.")
+
+    @commands.command()
     async def modstats(self, ctx, user: discord.User = None):
-        """Display moderation statistics for a user."""
+        """Display moderation statistics for a user or the invoking user."""
         if user is None:
             user = ctx.author
 
@@ -35,49 +61,7 @@ class ModeratorStatsCog(commands.Cog):
         seven_days_ago = datetime.utcnow() - timedelta(days=7)
         thirty_days_ago = datetime.utcnow() - timedelta(days=30)
 
-        mutes = await self.load_actions(guild, "mutes")
-        bans = await self.load_actions(guild, "bans")
-        kicks = await self.load_actions(guild, "kicks")
-        warns = await self.load_actions(guild, "warns")
-
-        action_counts = {
-            "mutes_7_days": sum(1 for mute in mutes if mute["timestamp"] >= seven_days_ago),
-            "mutes_30_days": sum(1 for mute in mutes if mute["timestamp"] >= thirty_days_ago),
-            "mutes_all_time": len(mutes),
-            "bans_7_days": sum(1 for ban in bans if ban["timestamp"] >= seven_days_ago),
-            "bans_30_days": sum(1 for ban in bans if ban["timestamp"] >= thirty_days_ago),
-            "bans_all_time": len(bans),
-            "kicks_7_days": sum(1 for kick in kicks if kick["timestamp"] >= seven_days_ago),
-            "kicks_30_days": sum(1 for kick in kicks if kick["timestamp"] >= thirty_days_ago),
-            "kicks_all_time": len(kicks),
-            "warns_7_days": sum(1 for warn in warns if warn["timestamp"] >= seven_days_ago),
-            "warns_30_days": sum(1 for warn in warns if warn["timestamp"] >= thirty_days_ago),
-            "warns_all_time": len(warns),
-            "total_7_days": 0,
-            "total_30_days": 0,
-            "total_all_time": 0
-        }
-
-        action_counts["total_7_days"] = (
-            action_counts["mutes_7_days"]
-            + action_counts["bans_7_days"]
-            + action_counts["kicks_7_days"]
-            + action_counts["warns_7_days"]
-        )
-
-        action_counts["total_30_days"] = (
-            action_counts["mutes_30_days"]
-            + action_counts["bans_30_days"]
-            + action_counts["kicks_30_days"]
-            + action_counts["warns_30_days"]
-        )
-
-        action_counts["total_all_time"] = (
-            action_counts["mutes_all_time"]
-            + action_counts["bans_all_time"]
-            + action_counts["kicks_all_time"]
-            + action_counts["warns_all_time"]
-        )
+        action_counts = await self.get_action_counts(guild)
 
         embed = discord.Embed(title="Moderation Statistics", color=discord.Color.green())
         embed.add_field(name="Mutes (last 7 days)", value=action_counts["mutes_7_days"], inline=True)
@@ -98,54 +82,81 @@ class ModeratorStatsCog(commands.Cog):
 
         await ctx.send(embed=embed)
 
-    async def load_actions(self, guild, action_type):
-        # Define the path for the JSON file for the guild's actions
-        guild_dir = os.path.join(self.data_dir, str(guild.id))
-        action_file = os.path.join(guild_dir, f"{action_type}.json")
+    async def get_action_counts(self, guild):
+        action_counts = {
+            "mutes_7_days": 0,
+            "mutes_30_days": 0,
+            "mutes_all_time": 0,
+            "bans_7_days": 0,
+            "bans_30_days": 0,
+            "bans_all_time": 0,
+            "kicks_7_days": 0,
+            "kicks_30_days": 0,
+            "kicks_all_time": 0,
+            "warns_7_days": 0,
+            "warns_30_days": 0,
+            "warns_all_time": 0,
+            "total_7_days": 0,
+            "total_30_days": 0,
+            "total_all_time": 0,
+        }
 
-        # Create the guild directory if it doesn't exist
-        os.makedirs(guild_dir, exist_ok=True)
+        try:
+            with open(os.path.join(self.data_dir, f"{guild.id}.json"), "r") as file:
+                data = json.load(file)
+                action_counts["mutes_7_days"] = data.get("mutes_7_days", 0)
+                action_counts["mutes_30_days"] = data.get("mutes_30_days", 0)
+                action_counts["mutes_all_time"] = data.get("mutes_all_time", 0)
+                action_counts["bans_7_days"] = data.get("bans_7_days", 0)
+                action_counts["bans_30_days"] = data.get("bans_30_days", 0)
+                action_counts["bans_all_time"] = data.get("bans_all_time", 0)
+                action_counts["kicks_7_days"] = data.get("kicks_7_days", 0)
+                action_counts["kicks_30_days"] = data.get("kicks_30_days", 0)
+                action_counts["kicks_all_time"] = data.get("kicks_all_time", 0)
+                action_counts["warns_7_days"] = data.get("warns_7_days", 0)
+                action_counts["warns_30_days"] = data.get("warns_30_days", 0)
+                action_counts["warns_all_time"] = data.get("warns_all_time", 0)
+                action_counts["total_7_days"] = (
+                    data.get("mutes_7_days", 0)
+                    + data.get("bans_7_days", 0)
+                    + data.get("kicks_7_days", 0)
+                    + data.get("warns_7_days", 0)
+                )
+                action_counts["total_30_days"] = (
+                    data.get("mutes_30_days", 0)
+                    + data.get("bans_30_days", 0)
+                    + data.get("kicks_30_days", 0)
+                    + data.get("warns_30_days", 0)
+                )
+                action_counts["total_all_time"] = (
+                    data.get("mutes_all_time", 0)
+                    + data.get("bans_all_time", 0)
+                    + data.get("kicks_all_time", 0)
+                    + data.get("warns_all_time", 0)
+                )
+        except FileNotFoundError:
+            pass
 
-        # Load the JSON file or create an empty list if it doesn't exist
-        if os.path.exists(action_file):
-            with open(action_file, "r") as file:
-                return json.load(file)
-        else:
-            return []
-
-    async def save_actions(self, guild, action_type, actions):
-        # Define the path for the JSON file for the guild's actions
-        guild_dir = os.path.join(self.data_dir, str(guild.id))
-        action_file = os.path.join(guild_dir, f"{action_type}.json")
-
-        # Create the guild directory if it doesn't exist
-        os.makedirs(guild_dir, exist_ok=True)
-
-        # Save the actions to the JSON file
-        with open(action_file, "w") as file:
-            json.dump(actions, file)
-
-    @commands.Cog.listener()
-    async def on_member_ban(self, guild, user):
-        await self.log_action(guild, "bans")
-
-    @commands.Cog.listener()
-    async def on_member_unban(self, guild, user):
-        await self.log_action(guild, "unbans")
+        return action_counts
 
     async def log_action(self, guild, action_type):
         timestamp = datetime.utcnow()
-        action = {"timestamp": timestamp, "type": action_type}
+        data_file = os.path.join(self.data_dir, f"{guild.id}.json")
 
-        if action_type == "bans":
-            async with self.config.guild(guild).bans() as bans:
-                bans.append(action)
-        elif action_type == "unbans":
-            async with self.config.guild(guild).bans() as bans:
-                # Remove the corresponding ban action based on your implementation
-                pass  # Add your logic here
+        try:
+            with open(data_file, "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            data = {}
 
-        # Implement similar logic for mutes, kicks, and warns
+        if action_type not in data:
+            data[action_type] = []
+
+        data[action_type].append({"timestamp": timestamp})
+        action_counts = await self.get_action_counts(guild)
+
+        with open(data_file, "w") as file:
+            json.dump(data, file, indent=4)
 
 def setup(bot):
     bot.add_cog(ModeratorStatsCog(bot))
