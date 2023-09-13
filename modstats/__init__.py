b51254b13acaf45199c8fc007e67c888019feba4
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-09-13 21:24:51 +0200

    Update __init__.py

--- modstats/__init__.py
+++ modstats/__init__.py
@@ -1,4 +1,4 @@
 from .modstats import ModeratorStatsCog
 
 async def setup(bot):
-    bot.add_cog(ModeratorStatsCog(bot))
+    bot.add_cog(ModeratorStatsCog(bot, bot.command_prefix))  # Pass the command prefix
