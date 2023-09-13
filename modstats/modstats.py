b1dd444bb56f810dca44fead78a0963a1ddb994f
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-09-13 16:49:40 +0200

    Update modstats.py

--- modstats/modstats.py
+++ modstats/modstats.py
@@ -10,10 +10,7 @@ class ModeratorStatsCog(commands.Cog):
         self.config = Config.get_conf(self, identifier=1234567890)  # Change identifier
 
         default_guild_settings = {
-            "mutes": [],
-            "bans": [],
-            "kicks": [],
-            "warns": []
+            "mod_actions": []  # Store all moderation actions in a single list
         }
 
         self.config.register_guild(**default_guild_settings)
@@ -29,68 +26,30 @@ class ModeratorStatsCog(commands.Cog):
         seven_days_ago = datetime.utcnow() - timedelta(days=7)
         thirty_days_ago = datetime.utcnow() - timedelta(days=30)
 
-        mutes = await self.config.guild(guild).mutes()
-        bans = await self.config.guild(guild).bans()
-        kicks = await self.config.guild(guild).kicks()
-        warns = await self.config.guild(guild).warns()
+        mod_actions = await self.config.guild(guild).mod_actions()
 
-        mutes_7_days = sum(1 for mute in mutes if mute["timestamp"] >= seven_days_ago)
-        mutes_30_days = sum(1 for mute in mutes if mute["timestamp"] >= thirty_days_ago)
-        mutes_all_time = len(mutes)
-
-        bans_7_days = sum(1 for ban in bans if ban["timestamp"] >= seven_days_ago)
-        bans_30_days = sum(1 for ban in bans if ban["timestamp"] >= thirty_days_ago)
-        bans_all_time = len(bans)
-
-        kicks_7_days = sum(1 for kick in kicks if kick["timestamp"] >= seven_days_ago)
-        kicks_30_days = sum(1 for kick in kicks if kick["timestamp"] >= thirty_days_ago)
-        kicks_all_time = len(kicks)
-
-        warns_7_days = sum(1 for warn in warns if warn["timestamp"] >= seven_days_ago)
-        warns_30_days = sum(1 for warn in warns if warn["timestamp"] >= thirty_days_ago)
-        warns_all_time = len(warns)
-
-        total_7_days = mutes_7_days + bans_7_days + kicks_7_days + warns_7_days
-        total_30_days = mutes_30_days + bans_30_days + kicks_30_days + warns_30_days
-        total_all_time = mutes_all_time + bans_all_time + kicks_all_time + warns_all_time
+        mod_actions_7_days = sum(1 for action in mod_actions if action["timestamp"] >= seven_days_ago)
+        mod_actions_30_days = sum(1 for action in mod_actions if action["timestamp"] >= thirty_days_ago)
+        mod_actions_all_time = len(mod_actions)
 
         embed = discord.Embed(title="Moderation Statistics", color=discord.Color.green())
-        embed.add_field(name="Mutes (last 7 days)", value=mutes_7_days, inline=True)
-        embed.add_field(name="Mutes (last 30 days)", value=mutes_30_days, inline=True)
-        embed.add_field(name="Mutes (all time)", value=mutes_all_time, inline=True)
-        embed.add_field(name="Bans (last 7 days)", value=bans_7_days, inline=True)
-        embed.add_field(name="Bans (last 30 days)", value=bans_30_days, inline=True)
-        embed.add_field(name="Bans (all time)", value=bans_all_time, inline=True)
-        embed.add_field(name="Kicks (last 7 days)", value=kicks_7_days, inline=True)
-        embed.add_field(name="Kicks (last 30 days)", value=kicks_30_days, inline=True)
-        embed.add_field(name="Kicks (all time)", value=kicks_all_time, inline=True)
-        embed.add_field(name="Warns (last 7 days)", value=warns_7_days, inline=True)
-        embed.add_field(name="Warns (last 30 days)", value=warns_30_days, inline=True)
-        embed.add_field(name="Warns (all time)", value=warns_all_time, inline=True)
-        embed.add_field(name="Total (last 7 days)", value=total_7_days, inline=True)
-        embed.add_field(name="Total (last 30 days)", value=total_30_days, inline=True)
-        embed.add_field(name="Total (all time)", value=total_all_time, inline=True)
+        embed.add_field(name="Actions (last 7 days)", value=mod_actions_7_days, inline=True)
+        embed.add_field(name="Actions (last 30 days)", value=mod_actions_30_days, inline=True)
+        embed.add_field(name="Actions (all time)", value=mod_actions_all_time, inline=True)
 
         await ctx.send(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_ban(self, guild, user):
-        await self.log_action(guild, "bans")
+        await self.log_action(guild, "ban")
 
     @commands.Cog.listener()
     async def on_member_unban(self, guild, user):
-        await self.log_action(guild, "unbans")
+        await self.log_action(guild, "unban")
 
     async def log_action(self, guild, action_type):
         timestamp = datetime.utcnow()
         action = {"timestamp": timestamp, "type": action_type}
 
-        if action_type == "bans":
-            async with self.config.guild(guild).bans() as bans:
-                bans.append(action)
-        elif action_type == "unbans":
-            async with self.config.guild(guild).bans() as bans:
-                # Remove the corresponding ban action based on your implementation
-                pass  # Add your logic here
-
-        # Implement similar logic for mutes, kicks, and warns
+        async with self.config.guild(guild).mod_actions() as mod_actions:
+            mod_actions.append(action)
