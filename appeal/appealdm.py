6f6019b3cf01266ce4be18919fac784ad43a7160
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-09-15 19:55:16 +0200

    Update appealdm.py

--- appeal/appealdm.py
+++ appeal/appealdm.py
@@ -10,8 +10,9 @@ class AppealDM(commands.Cog):
         """Send ban appeal form."""
         embed = discord.Embed(
             title='Ban appeal form for Joe Caine & Co',
-            description='Click the link below for the form.\n\n[https://forms.gle/hm6EwyKdCFsn8opQA](https://forms.gle/hm6EwyKdCFsn8opQA)',
+            description='Click the link below for the form.\n\nhttps://forms.gle/hm6EwyKdCFsn8opQA',
             colour=15158332,
+            url='https://forms.gle/hm6EwyKdCFsn8opQA',
         )
 
         embed.set_author(
@@ -19,11 +20,7 @@ class AppealDM(commands.Cog):
             icon_url='https://images-ext-2.discordapp.net/external/WpNkZdR9ewGNudG8uTVXu33m-Yqo2qEVr5VZrA_hCcY/%3Fsize%3D1024/https/cdn.discordapp.com/icons/1090736013258789028/a_bc4b81914fd1795cbee691017a907c99.gif?width=1024&height=1024'
         )
 
-        try:
-            await user.send(embed=embed)
-            await ctx.send(f"Ban appeal form has been sent to {user.name}.")
-        except discord.Forbidden:
-            await ctx.send("I couldn't send the appeal form. Make sure the user's DMs are open.")
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(AppealDM(bot))
