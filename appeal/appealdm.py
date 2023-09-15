8363d7d157ded60e358d2ad7dc1692850b76ae4d
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-09-15 19:46:01 +0200

    Update appealdm.py

--- appeal/appealdm.py
+++ appeal/appealdm.py
@@ -8,9 +8,14 @@ class AppealDM(commands.Cog):
     @commands.command()
     async def appeal(self, ctx, user: discord.User):
         """Send ban appeal form."""
-        appeal_form = "Ban appeal form for Joe Caine & Co\n\nhttps://forms.gle/hm6EwyKdCFsn8opQA"
+        embed = discord.Embed(
+            title="Ban Appeal Form",
+            description="Click the link below to access the ban appeal form.",
+            url="https://forms.gle/hm6EwyKdCFsn8opQA",
+            color=discord.Color.red(),
+        )
         try:
-            await user.send(appeal_form)
+            await user.send(embed=embed)
             await ctx.send(f"Ban appeal form has been sent to {user.name}.")
         except discord.Forbidden:
             await ctx.send("I couldn't send the appeal form. Make sure the user's DMs are open.")
