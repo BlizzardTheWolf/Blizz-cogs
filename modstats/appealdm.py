850a2a5c6d26db3f29d85d90011bdad22b59f044
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-09-15 19:24:09 +0200

    Update and rename modstats.py to appealdm.py

---
+++ modstats/appealdm.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+class AppealDM(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="appeal")
+    async def send_appeal(self, ctx, user: discord.User):
+        appeal_form_link = "https://forms.gle/hm6EwyKdCFsn8opQA"
+        
+        embed = discord.Embed(
+            title="Ban Appeal Form for Joe Caine & Co",
+            description=f"Click [here]({appeal_form_link}) to access the ban appeal form.",
+            color=discord.Color.blue()
+        )
+        
+        await user.send(embed=embed)
+        await ctx.send(f"Ban appeal form sent to {user.display_name}.")
+
+def setup(bot):
+    bot.add_cog(AppealDM(bot))
