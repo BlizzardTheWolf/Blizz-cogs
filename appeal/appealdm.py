ff751ed46fc0894d8ceee95c0c8953e72580c3e7
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-09-15 19:51:08 +0200

    Update appealdm.py

--- appeal/appealdm.py
+++ appeal/appealdm.py
@@ -9,13 +9,15 @@ class AppealDM(commands.Cog):
     async def appeal(self, ctx, user: discord.User):
         """Send ban appeal form."""
         embed = discord.Embed(
-            title="Ban Appeal Form",
-            description="Click the link below to access the ban appeal form.",
-            url="https://forms.gle/hm6EwyKdCFsn8opQA",
-            color=discord.Color.red(),
-        )
-        try:
-            await user.send(embed=embed)
+    title='Ban appeal form for Joe Caine & Co',
+    description='Click the link below for the form.\n\nhttps://forms.gle/hm6EwyKdCFsn8opQA',
+    colour=15158332,
+    url='https://forms.gle/hm6EwyKdCFsn8opQA',
+)
+
+embed.set_author(name='Joe Caine & Co', icon_url='https://images-ext-2.discordapp.net/external/WpNkZdR9ewGNudG8uTVXu33m-Yqo2qEVr5VZrA_hCcY/%3Fsize%3D1024/https/cdn.discordapp.com/icons/1090736013258789028/a_bc4b81914fd1795cbee691017a907c99.gif?width=1024&height=1024')
+
+await ctx.send(embed=embed)
             await ctx.send(f"Ban appeal form has been sent to {user.name}.")
         except discord.Forbidden:
             await ctx.send("I couldn't send the appeal form. Make sure the user's DMs are open.")
