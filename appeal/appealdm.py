9cdeb6d709639cc6bc4fc560a0fc9f638d8bb38c
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-09-15 19:56:19 +0200

    Update appealdm.py

--- appeal/appealdm.py
+++ appeal/appealdm.py
@@ -7,7 +7,7 @@ class AppealDM(commands.Cog):
 
     @commands.command()
     async def appeal(self, ctx, user: discord.User):
-        """Send ban appeal form."""
+        """Send ban appeal form to the specified user's DMs."""
         embed = discord.Embed(
             title='Ban appeal form for Joe Caine & Co',
             description='Click the link below for the form.\n\nhttps://forms.gle/hm6EwyKdCFsn8opQA',
@@ -20,7 +20,7 @@ class AppealDM(commands.Cog):
             icon_url='https://images-ext-2.discordapp.net/external/WpNkZdR9ewGNudG8uTVXu33m-Yqo2qEVr5VZrA_hCcY/%3Fsize%3D1024/https/cdn.discordapp.com/icons/1090736013258789028/a_bc4b81914fd1795cbee691017a907c99.gif?width=1024&height=1024'
         )
 
-        await ctx.send(embed=embed)
+        await user.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(AppealDM(bot))
