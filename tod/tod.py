1c101701cf3124d3e57f3406a52a505a0465975e
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-09-29 15:41:06 +0200

    Update tod.py

--- tod/tod.py
+++ tod/tod.py
@@ -1,44 +1,34 @@
 import discord
-from redbot.core import commands, checks
+from redbot.core import commands, checks, Config
 import random
-import json
-import asyncio
 
 class TruthOrDare(commands.Cog):
     """Truth or Dare Cog"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.config = bot.config
-
-    async def initialize_questions(self):
-        # Check if the JSON files exist, if not, initialize them with default questions
-        default_truth_questions = [
-            "What's the most embarrassing thing that's happened to you?",
-            "Have you ever lied to your best friend?",
-            "What's your secret talent?"
-        ]
-        default_dare_questions = [
-            "Do your best impersonation of a chicken for 1 minute.",
-            "Call a random contact in your phone and sing 'Happy Birthday.'",
-            "Wear socks on your hands for the next 5 minutes."
-        ]
-        default_wyr_questions = [
-            "Would you rather have the ability to fly or be invisible?",
-            "Would you rather always be 10 minutes late or 20 minutes early?",
-            "Would you rather live in a giant desert or a freezing tundra?"
-        ]
-
-        await self.config.truth_questions.set(default_truth_questions)
-        await self.config.dare_questions.set(default_dare_questions)
-        await self.config.wyr_questions.set(default_wyr_questions)
-
-    async def cog_check(self, ctx):
-        return ctx.guild is not None
+        self.config = Config.get_conf(self, identifier=12345544890)  # Use a unique identifier
+        default_global = {
+            "truth_questions": [
+                "What's the most embarrassing thing that's happened to you?",
+                "Have you ever lied to your best friend?",
+                "What's your secret talent?"
+            ],
+            "dare_questions": [
+                "Do your best impersonation of a chicken for 1 minute.",
+                "Call a random contact in your phone and sing 'Happy Birthday.'",
+                "Wear socks on your hands for the next 5 minutes."
+            ],
+            "wyr_questions": [
+                "Would you rather have the ability to fly or be invisible?",
+                "Would you rather always be 10 minutes late or 20 minutes early?",
+                "Would you rather live in a giant desert or a freezing tundra?"
+            ]
+        }
+        self.config.register_global(**default_global)
 
     @commands.Cog.listener()
     async def on_ready(self):
-        await self.initialize_questions()
         print("TruthOrDare cog is ready!")
 
     @commands.command()
