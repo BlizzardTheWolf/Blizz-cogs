97317518e03e449b8479b124aeacb177dbd7da32
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-03 22:27:19 +0200

    Update awoo.py

--- setup/awoo.py
+++ setup/awoo.py
@@ -8,7 +8,7 @@ class AwooCog(commands.Cog):
     @commands.command()
     async def setup(self, ctx):
         """Send the setup message."""
-        await ctx.send("I will now start setting up the bot in this server! You can check the commands with `;help`. Setting up the bot can take 10-30 minutes depending on size and payload. ")
+        await ctx.send("I will now start setting up the bot in this server! You can check the commands with `;help`, more commands will be available after setup. Setting up the bot can take 10-30 minutes depending on size and current server load. **Make sure that this bot has full administrator permissions, or setup will fail!**")
 
 def setup(bot):
     bot.add_cog(AwooCog(bot))
