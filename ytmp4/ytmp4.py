c3ae75d0e53df590de1d4dc8d9361da1d3f66e27
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-16 14:07:44 +0200

    Update ytmp4.py

--- ytmp4/ytmp4.py
+++ ytmp4/ytmp4.py
@@ -10,11 +10,6 @@ class YTMP4Cog(commands.Cog):
 
     @commands.command()
     async def convert(self, ctx, url, format="mp4"):
-        format = format.lower()  # Convert the format argument to lowercase
-        if format not in ["mp4", "mp3"]:
-            await ctx.send("Invalid format. Please use 'mp4' or 'mp3'.")
-            return
-
         try:
             yt = YouTube(url)
 
@@ -22,7 +17,7 @@ class YTMP4Cog(commands.Cog):
                 await ctx.send("This video is age-restricted and cannot be converted.")
                 return
 
-            stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+            stream = yt.streams.filter(progressive=True, file_extension=format).order_by('resolution').desc().first()
 
             if not stream:
                 await ctx.send("Could not find a suitable video stream for download.")
@@ -36,31 +31,18 @@ class YTMP4Cog(commands.Cog):
 
             await ctx.send("Converting the video, please wait...")
 
-            video_path = f'video-{ctx.author.id}.mp4'
-            stream.download(output_path='/mnt/converter', filename=video_path)
+            video_path = f'{yt.title}-{ctx.author.id}.{format}'
+            stream.download(output_path='/mnt/converter/', filename=video_path)
 
-            if os.path.getsize(video_path) > max_file_size_bytes:
+            if os.path.getsize(f'/mnt/converter/{video_path}') > max_file_size_bytes:
                 await ctx.send("The file is too big to be converted. It must be under 25MBs. This is Discord's fault, not mine.")
-                os.remove(video_path)
+                os.remove(f'/mnt/converter/{video_path}')
                 return
 
-            if format == "mp3":
-                mp3_path = f'audio-{ctx.author.id}.mp3'
+            await asyncio.sleep(5)
 
-                video_clip = mp.VideoFileClip(video_path)
-                audio_clip = video_clip.audio
-                audio_clip.write_audiofile(mp3_path)
-                audio_clip.close()
-                video_clip.close()
-
-                await ctx.send(f'{ctx.author.mention}, your video conversion is complete. Here is the converted audio:', file=discord.File(mp3_path))
-                os.remove(mp3_path)
-            else:
-                await asyncio.sleep(5)
-                user = ctx.message.author
-                await ctx.send(f'{user.mention}, your video conversion is complete. Here is the converted video:', file=discord.File(video_path))
-
-            os.remove(video_path)
+            user = ctx.message.author
+            await ctx.send(f'{user.mention}, your video conversion is complete. Here is the converted video:', file=discord.File(f'/mnt/converter/{video_path}'))
         except Exception as e:
             error_message = str(e)
             await ctx.send(f"An error occurred during video conversion. Please check the URL and try again.\nError details: {error_message}")
