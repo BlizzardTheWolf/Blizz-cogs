a3eabe16178d98b5cab78ad7acab6c3c756f91eb
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-16 14:14:06 +0200

    Update ytmp4.py

--- ytmp4/ytmp4.py
+++ ytmp4/ytmp4.py
@@ -47,4 +47,4 @@ class YTMP4Cog(commands.Cog):
             await ctx.send(f"An error occurred during video conversion. Please check the URL and try again.\nError details: {error_message}")
 
 def setup(bot):
-    bot.add_cog(YTMP4Cog(bot)
+    bot.add_cog(YTMP4Cog(bot))
