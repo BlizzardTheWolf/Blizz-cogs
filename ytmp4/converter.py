c3da453947c37af4812fe94bfcfdb35318da9610
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-16 14:30:20 +0200

    Update converter.py

--- ytmp4/converter.py
+++ ytmp4/converter.py
@@ -1,76 +1,65 @@
+import os
 import discord
-from discord.ext import commands
+from redbot.core import commands
 from pytube import YouTube
 import asyncio
-import os
-import traceback
-import datetime
+from datetime import timezone
 
-intents = discord.Intents.default()
-intents.typing = False
-intents.presences = False
-intents.message_content = True
-bot = commands.Bot(command_prefix='!', intents=intents)
+class YTMP4Cog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-maintenance_user_id = 1071559067975295058
-maintenance_mode = False
-max_video_duration = 600
-max_file_size_bytes = 25 * 1024 * 1024
+        if not os.path.exists('/mnt/converter'):
+            os.makedirs('/mnt/converter')
 
-if not os.path.exists('error_log.txt'):
-    with open('error_log.txt', 'w'):
-        pass
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print(f'Logged in as {self.bot.user.name}')
 
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-    await bot.change_presence(activity=discord.Game(name="!convert"))
+    @commands.command()
+    async def convert(self, ctx, url, format='mp4'):
+        try:
+            yt = YouTube(url)
 
-@bot.event
-async def on_error(event, *args, **kwargs):
-    error_message = traceback.format_exc()
-    print(f'Bot went down due to an error at {datetime.datetime.now()}:\n{error_message}')
-    with open('error_log.txt', 'w') as error_log:
-        error_log.write(error_message)
+            if yt.age_restricted:
+                await ctx.send("This video is age-restricted and cannot be converted.")
+                return
 
-@bot.command()
-async def convert(ctx, url, filetype="mp4"):
-    try:
-        yt = YouTube(url)
+            stream = yt.streams.filter(progressive=True, file_extension=format).order_by('resolution').desc().first()
 
-        if yt.age_restricted:
-            await ctx.send("This video is age-restricted and cannot be converted.")
-            return
+            if not stream:
+                await ctx.send("Could not find a suitable video stream for download.")
+                return
 
-        stream = yt.streams.filter(progressive=True, file_extension=filetype).order_by('resolution').desc().first()
+            duration = yt.length
 
-        if not stream:
-            await ctx.send("Could not find a suitable video stream for download.")
-            return
+            if duration > 600:
+                await ctx.send("Video exceeds the maximum time limit of 10 minutes.")
+                return
 
-        duration = yt.length
+            await ctx.trigger_typing()  # Show "typing" status while converting
 
-        if duration > max_video_duration:
-            await ctx.send("Video exceeds the maximum time limit of 10 minutes.")
-            return
+            video_path = f'/mnt/converter/{yt.title}-{ctx.author.id}.{format}'
+            stream.download(output_path='/mnt/converter', filename=yt.title)
+            file_size = os.path.getsize(video_path)
 
-        await ctx.trigger_typing()  # Show "typing" status while converting
+            if file_size > 25 * 1024 * 1024:  # 25MB
+                os.remove(video_path)
+                await ctx.send("The file is too big to be converted. It must be under 25MBs. This is Discord's fault, not mine.")
+                return
 
-        await ctx.send("Converting the video, please wait...")
-        video_path = 'video.mp4'
-        stream.download(output_path='/mnt/converter', filename=f"{yt.title}-{ctx.author.id}")
+            if format == 'mp3':
+                os.system(f'ffmpeg -i "{video_path}" -vn -ab 192k -ar 44100 -y "{video_path[:-4]}.mp3"')
+                os.remove(video_path)
+                video_path = f'/mnt/converter/{yt.title}-{ctx.author.id}.mp3'
 
-        if os.path.getsize(video_path) > max_file_size_bytes:
-            await ctx.send("The file is too big to be converted. It must be under 25MBs. This is Discord's fault, not mine.")
-            os.remove(video_path)
-            return
+            user = ctx.author
+            await ctx.send(f'{user.mention}, your video conversion is complete. Here is the converted file:', file=discord.File(video_path))
+        except Exception as e:
+            error_message = str(e)
+            await ctx.send(f"An error occurred during video conversion. Please check the URL and try again.\nError details: {error_message}")
 
-        await asyncio.sleep(5)
+    # ... (other bot commands, if any)
 
-        user = ctx.message.author
-        await ctx.send(f'{user.mention}, your video conversion is complete. Here is the converted video:', file=discord.File(video_path))
-    except Exception as e:
-        error_message = str(e)
-        await ctx.send(f"An error occurred during video conversion. Please check the URL and try again.\nError details: {error_message}")
-
-# ...
+def setup(bot):
+    bot.add_cog(YTMP4Cog(bot))
