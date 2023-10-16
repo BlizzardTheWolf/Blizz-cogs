7dee681e55bb19bbf48e4fab9650592c7cd9a303
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-16 16:10:30 +0200

    Update ytmp3.py

--- ytmp3/ytmp3.py
+++ ytmp3/ytmp3.py
@@ -2,11 +2,7 @@ import os
 import discord
 from redbot.core import commands
 import asyncio
-from pytube import YouTube
-from moviepy.editor import *
-from moviepy.config import change_settings
-
-change_settings({"FFMPEG_BINARY": "/usr/bin/ffmpeg"})  # Update the path to the FFMPEG binary
+import youtube_dl
 
 class YTMP3Cog(commands.Cog):
     def __init__(self, bot):
@@ -15,43 +11,34 @@ class YTMP3Cog(commands.Cog):
     @commands.command()
     async def ytmp3(self, ctx, url):
         try:
-            yt = YouTube(url)
-            
-            # Get the best audio stream
-            stream = yt.streams.filter(only_audio=True).first()
-            if not stream:
-                await ctx.send("Could not find a suitable audio stream for download.")
-                return
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+                'outtmpl': f'/mnt/converter/%(title)s-{ctx.author.id}.%(ext)s',
+            }
 
-            await ctx.send("Downloading the audio, please wait...")
-            
-            # Generate a unique filename based on video title and user ID
-            video_title = f"{yt.title}-{ctx.author.id}"
-            
-            # Download the video stream
-            stream.download(output_path="/mnt/converter", filename=f"{video_title}.temp")
-            
-            # Handle the 'video_fps' issue in MoviePy
-            try:
-                clip = VideoFileClip(f"/mnt/converter/{video_title}.temp")
-            except Exception as e:
-                # VideoFileClip may fail due to the 'video_fps' issue, so we'll try another method
-                clip = VideoFileClip(f"/mnt/converter/{video_title}.temp", audio=False)
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(url, download=False)
+                if 'video_fps' not in info_dict:
+                    await ctx.send("The video format is not compatible with audio conversion.")
+                    return
 
-            # Convert the video to audio
-            clip.audio.write_audiofile(f"/mnt/converter/{video_title}.mp3")
-            
-            # Remove the temporary video file
-            os.remove(f"/mnt/converter/{video_title}.temp")
-            
+                await ctx.send("Converting the video to MP3, please wait...")
+
+                ydl.download([url])
+
             await asyncio.sleep(5)
             user = ctx.message.author
-            await ctx.send(f"{user.mention}, your audio conversion is complete. Here is the converted audio:",
-                           file=discord.File(f"/mnt/converter/{video_title}.mp3"))
+            converted_filename = f"{info_dict.get('title', 'audio')}-{ctx.author.id}.mp3"
+            await ctx.send(f'{user.mention}, your video conversion to MP3 is complete. Here is the converted audio:', file=discord.File(f"/mnt/converter/{converted_filename}"))
 
-            # Remove the audio file after 10 minutes
+            # Remove the file after 10 minutes
             await asyncio.sleep(600)
-            os.remove(f"/mnt/converter/{video_title}.mp3")
+            os.remove(f"/mnt/converter/{converted_filename}")
 
         except Exception as e:
             error_message = str(e)
