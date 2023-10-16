305d8d420252803261e98dbb7dbe72fc1308d6be
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-16 16:31:05 +0200

    Update ytmp3.py

--- ytmp3/ytmp3.py
+++ ytmp3/ytmp3.py
@@ -4,8 +4,6 @@ from redbot.core import commands
 from pytube import YouTube
 from moviepy.editor import VideoFileClip
 import asyncio
-import re
-from unidecode import unidecode
 
 class YTMP3Cog(commands.Cog):
     def __init__(self, bot):
@@ -28,21 +26,14 @@ class YTMP3Cog(commands.Cog):
 
             await ctx.send("Converting the video to MP3, please wait...")
 
-            # Get the video title directly from YouTube
-            title = yt.title
+            # Get the video code from the YouTube URL
+            video_code = yt.video_id
 
-            # Sanitize the title to remove special characters
-            title = unidecode(title)  # Convert to ASCII characters
-            title = re.sub(r'[\/:*?"<>|]', '_', title)  # Replace illegal filename characters with underscores
-            title = re.sub(r'_+', '_', title)  # Replace consecutive underscores with a single underscore
-            title = title.strip('_')  # Remove leading/trailing underscores
-            title = title[:100]  # Limit the filename length to avoid issues
-
-            video_path = f'/mnt/converter/{title}-{ctx.author.id}.mp4'
-            audio_path = f'/mnt/converter/{title}-{ctx.author.id}.mp3'
+            video_path = f'/mnt/converter/{video_code}.mp4'
+            audio_path = f'/mnt/converter/{video_code}.mp3'
 
             video_stream = yt.streams.get_highest_resolution()
-            video_stream.download(output_path="/mnt/converter", filename=title)
+            video_stream.download(output_path="/mnt/converter", filename=video_code)
 
             clip = VideoFileClip(video_path)
             clip.audio.write_audiofile(audio_path)
