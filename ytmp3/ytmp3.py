376e46cff4697ab769f940a26d4a812e0db5e164
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-16 15:36:28 +0200

    Update ytmp3.py

--- ytmp3/ytmp3.py
+++ ytmp3/ytmp3.py
@@ -2,6 +2,7 @@ import os
 import discord
 from redbot.core import commands
 import asyncio
+import youtube_dl
 
 class YTMP3Cog(commands.Cog):
     def __init__(self, bot):
@@ -10,29 +11,35 @@ class YTMP3Cog(commands.Cog):
     @commands.command()
     async def ytmp3(self, ctx, url):
         try:
-            mp3_filename = await self.download_mp3(url)
+            ydl_opts = {
+                'format': 'bestaudio/best',
+                'postprocessors': [{
+                    'key': 'FFmpegExtractAudio',
+                    'preferredcodec': 'mp3',
+                    'preferredquality': '192',
+                }],
+                'outtmpl': f'/mnt/converter/%(title)s-{ctx.author.id}.%(ext)s',
+            }
 
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(url, download=False)
+                video_title = info_dict.get('title', 'video')
+
+                await ctx.send("Converting the video to MP3, please wait...")
+
+                ydl.download([url])
+
+            await asyncio.sleep(5)
             user = ctx.message.author
-            await ctx.send(f'{user.mention}, your video conversion to MP3 is complete. Here is the MP3 audio:', file=discord.File(mp3_filename))
+            await ctx.send(f'{user.mention}, your video conversion to MP3 is complete. Here is the converted audio:', file=discord.File(f"/mnt/converter/{video_title}-{ctx.author.id}.mp3"))
 
             # Remove the file after 10 minutes
             await asyncio.sleep(600)
-            os.remove(mp3_filename)
+            os.remove(f"/mnt/converter/{video_title}-{ctx.author.id}.mp3")
 
         except Exception as e:
             error_message = str(e)
             await ctx.send(f"An error occurred during audio conversion. Please check the URL and try again.\nError details: {error_message}")
-
-    async def download_mp3(self, url):
-        mp3_filename = "/mnt/converter/audio.mp3"  # Adjust the filename and path as needed
-        command = f"yt-dl --extract-audio --audio-format mp3 -o '{mp3_filename}' '{url}'"
-
-        try:
-            # Run the yt-dl command to download and convert the video to MP3
-            os.system(command)
-            return mp3_filename
-        except Exception as e:
-            raise e
 
 def setup(bot):
     bot.add_cog(YTMP3Cog(bot))
