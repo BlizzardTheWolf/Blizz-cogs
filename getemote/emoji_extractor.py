957b5000c332a18ba321220991e257b18bafb2ea
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-18 16:12:50 +0200

    Update emoji_extractor.py

--- getemote/emoji_extractor.py
+++ getemote/emoji_extractor.py
@@ -25,14 +25,23 @@ class GetEmoteCog(commands.Cog):
 
         await super().on_message(message)
 
+    @commands.Cog.listener()
+    async def on_message_without_command(self, message: discord.Message):
+        if hasattr(message, "custom_emotes") and message.author != self.bot.user:
+            # Automatically send the embed when the ;getemote command is not required
+            await self.send_emote_embed(message)
+
     @commands.command()
     async def getemote(self, ctx: commands.Context):
         if hasattr(ctx.message, "custom_emotes"):
-            emote_info = ctx.message.custom_emotes
-            embed = discord.Embed(title="Custom Emote Links")
-            for emote_name, emote_url in emote_info.items():
-                embed.add_field(name=emote_name, value=emote_url, inline=False)
-            await ctx.send(embed=embed)
+            await self.send_emote_embed(ctx.message)
+
+    async def send_emote_embed(self, message: discord.Message):
+        emote_info = message.custom_emotes
+        embed = discord.Embed(title="Custom Emote Links")
+        for emote_name, emote_url in emote_info.items():
+            embed.add_field(name=emote_name, value=emote_url, inline=False)
+        await message.channel.send(embed=embed)
 
 def setup(bot: Red):
     bot.add_cog(GetEmoteCog(bot))
