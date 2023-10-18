560f7936da96310c00a8c8642f62a5286ff2e748
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-18 15:57:13 +0200

    Update emote_extractor.py

--- getemote/emote_extractor.py
+++ getemote/emote_extractor.py
@@ -1,5 +1,6 @@
-import discord
 from redbot.core import commands
+import discord
+import re
 
 class EmoteExtractorCog(commands.Cog):
     def __init__(self, bot):
@@ -7,34 +8,30 @@ class EmoteExtractorCog(commands.Cog):
 
     @commands.command()
     async def getemote(self, ctx):
-        message = ctx.message
-        if message.reference:
-            # Check if the message is a reply
-            replied_message = await message.channel.fetch_message(message.reference.message_id)
-            emoji_codes = self.extract_emoji_codes(replied_message.content)
+        emote_codes = re.findall(r':\w+:', ctx.message.content)
+        emote_urls = []
+
+        for emote_code in emote_codes:
+            emoji = self.get_custom_emoji(ctx, emote_code)
+            if emoji:
+                emote_urls.append(str(emoji.url))
+
+        if emote_urls:
+            emote_links = "\n".join(emote_urls)
+            embed = discord.Embed(
+                title="Custom Emoji Links",
+                description=emote_links,
+                color=discord.Color.blue()
+            )
+            await ctx.send(embed=embed)
         else:
-            emoji_codes = self.extract_emoji_codes(message.content)
+            await ctx.send("No custom emojis found in the message.")
 
-        if not emoji_codes:
-            await ctx.send("No custom emoji found in the message.")
-            return
-
-        embed = discord.Embed(title="Custom Emoji Links", color=discord.Color.blurple())
-        for emoji_code in emoji_codes:
-            emoji_url = discord.Embed.Empty
-            for emoji in self.bot.emojis:
-                if emoji.name == emoji_code:
-                    emoji_url = emoji.url
-                    break
-
-            embed.add_field(name=f":{emoji_code}:", value=emoji_url, inline=False)
-
-        await ctx.send(embed=embed)
-
-    def extract_emoji_codes(self, text):
-        # Extract custom emoji codes from the text
-        import re
-        return re.findall(r':[a-zA-Z0-9_]+:', text)
+    def get_custom_emoji(self, ctx, emote_code):
+        emote_id = re.search(r'\d+', emote_code)
+        if emote_id:
+            emote = discord.utils.get(ctx.guild.emojis, id=int(emote_id.group(0)))
+            return emote
 
 def setup(bot):
     bot.add_cog(EmoteExtractorCog(bot))
