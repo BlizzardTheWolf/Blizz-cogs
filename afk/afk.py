811f7ed1a802d7577fcd96cca9f42948d55cac27
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-20 17:53:10 +0200

    Update afk.py

--- afk/afk.py
+++ afk/afk.py
@@ -1,56 +1,46 @@
 import discord
 from redbot.core import commands
-from redbot.core.utils.chat_formatting import humanize_list
-from redbot.core import checks
 
 class AFK(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.afk_statuses = {}  # Dictionary to store AFK statuses
-
-    @commands.command()
-    async def afk(self, ctx, *, reason: str = "AFK"):
-        """Set your AFK status with an optional reason."""
-        member = ctx.author
-        self.afk_statuses[member.id] = reason
-        await member.edit(nick=f"[AFK] {member.display_name}")
-        await ctx.send(f"{ctx.author.mention} is now AFK: {reason}")
+        self.afk_users = set()
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        author = message.author
-        if author.id in self.afk_statuses:
-            del self.afk_statuses[author.id]  # Clear AFK status upon sending a message
-            await author.edit(nick=author.display_name.lstrip("[AFK] "))
+        if message.author.id in self.afk_users:
+            await self.clear_afk(message.author)
+            await message.channel.send(f"{message.author.mention} is no longer AFK.")
+
+    async def clear_afk(self, user):
+        if user.id in self.afk_users:
+            self.afk_users.remove(user.id)
+            try:
+                await user.edit(nick=user.display_name.replace("[AFK] ", ""))
+            except discord.Forbidden:
+                pass
+
+    @commands.command()
+    async def afk(self, ctx):
+        if ctx.author.id not in self.afk_users:
+            self.afk_users.add(ctx.author.id)
+            try:
+                await ctx.author.edit(nick=f"[AFK] {ctx.author.display_name}")
+            except discord.Forbidden:
+                pass
+            await ctx.send(f"{ctx.author.mention} is now AFK.")
 
     @commands.Cog.listener()
-    async def on_message_without_command(self, message):
-        if message.mentions:
-            afk_mentions = [
-                member.mention for member in message.mentions if member.id in self.afk_statuses
-            ]
-            if afk_mentions:
-                afk_list = humanize_list(afk_mentions)
-                await message.channel.send(f"{afk_list} {'is' if len(afk_mentions) == 1 else 'are'} AFK.")
+    async def on_message(self, message):
+        if message.author.id in self.afk_users:
+            await self.clear_afk(message.author)
+            await message.channel.send(f"{message.author.mention} is no longer AFK.")
+            self.afk_users.discard(message.author.id)  # Clear AFK status
 
     @commands.command()
-    @checks.mod_or_permissions(manage_messages=True)  # Modify the permissions as needed
-    async def clearafk(self, ctx, member: discord.Member):
-        """Clear the AFK status of a member."""
-        if member.id in self.afk_statuses:
-            del self.afk_statuses[member.id]
-            await member.edit(nick=member.display_name.lstrip("[AFK] "))
-            await ctx.send(f"AFK status cleared for {member.mention}.")
-
-    @commands.command()
-    @checks.mod_or_permissions(manage_messages=True)  # Modify the permissions as needed
-    async def listafk(self, ctx):
-        """List all members with AFK status."""
-        afk_members = [ctx.guild.get_member(member_id) for member_id in self.afk_statuses.keys()]
-        afk_members = [member.mention for member in afk_members if member]
-        afk_list = humanize_list(afk_members) if afk_members else "No members are currently AFK."
-        await ctx.send(f"Members with AFK status: {afk_list}")
+    async def notafk(self, ctx):
+        await self.clear_afk(ctx.author)
+        await ctx.send(f"{ctx.author.mention} is no longer AFK.")
 
 def setup(bot):
-    afk_cog = AFK(bot)
-    bot.add_cog(afk_cog)
+    bot.add_cog(AFK(bot))
