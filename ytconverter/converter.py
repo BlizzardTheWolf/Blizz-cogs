900ddde3c262851b6b5bbd3c42ae042ba67058f0
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-27 21:22:23 +0200

    Update converter.py

--- ytconverter/converter.py
+++ ytconverter/converter.py
@@ -62,7 +62,7 @@ class ConverterCog(commands.Cog):
 
             user = ctx.message.author
             await ctx.send(f'{user.mention}, your video conversion to {output_ext} is complete. Here is the converted file:',
-                file=discord.File(str(video_path))
+                           file=discord.File(str(video_path))
 
             # Remove the file after 10 minutes
             await asyncio.sleep(600)
