31df758caef39add5eb10457725a17b19527d102
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-27 20:25:05 +0200

    Update converter.py

--- ytconverter/converter.py
+++ ytconverter/converter.py
@@ -81,7 +81,7 @@ class ConverterCog(commands.Cog):
             await asyncio.sleep(5)
 
             user = ctx.message.author
-            await ctx.send(f'{user.mention}, your video conversion to mp4 is complete. Here is the converted video:', file=discord.File(str(video_path))
+            await ctx.send(f'{user.mention}, your video conversion to mp4 is complete. Here is the converted video:', file=discord.File(str(video_path)))
 
             # Remove the file after 10 minutes
             await asyncio.sleep(600)
