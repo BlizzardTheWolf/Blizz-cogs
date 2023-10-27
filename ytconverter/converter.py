59c51a6423aaabd106affe0eac9bd45e46f3911f
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-27 19:21:48 +0200

    Update converter.py

--- ytconverter/converter.py
+++ ytconverter/converter.py
@@ -1,21 +1,42 @@
-import os
 import discord
 from redbot.core import commands
-from pytube import YouTube
-import aiohttp
+from youtube_dl import YoutubeDL
+import os
 import asyncio
 import time
 
 class ConverterCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.data_path = self.bot.cog_data_path(self)
+        self.converter_dir = os.path.join(str(self.bot.cog_data_path(self)), "downloads")
+
+    @commands.Cog.listener()
+    async def cog_check(self, ctx):
+        if not ctx.author.bot:
+            return True
+
+    async def download_and_convert(self, ctx, url, only_audio):
+        ydl_opts = {
+            "format": "bestaudio" if only_audio else "best",
+            "outtmpl": os.path.join(self.converter_dir, "%(title)s.%(ext)s"),
+        }
+        with YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = os.path.join(self.converter_dir, info["_filename"])
+            return filename
 
     @commands.command()
     async def ytmp3(self, ctx, url):
         try:
             await ctx.send("Converting the video to MP3, please wait...")
-            await self.download_and_convert(ctx, url, only_audio=True)
+            audio_path = await self.download_and_convert(ctx, url, only_audio=True)
+
+            user = ctx.author
+            await ctx.send(f'{user.mention}, your video conversion to MP3 is complete. Here is the converted audio:', file=discord.File(audio_path))
+
+            # Remove the file after 10 minutes
+            await asyncio.sleep(600)
+            os.remove(audio_path)
 
         except Exception as e:
             error_message = str(e)
@@ -25,112 +46,15 @@ class ConverterCog(commands.Cog):
     async def ytmp4(self, ctx, url):
         try:
             await ctx.send("Converting the video to mp4, please wait...")
-            await self.download_and_convert(ctx, url, only_audio=False)
+            video_path = await self.download_and_convert(ctx, url, only_audio=False)
+
+            user = ctx.author
+            await ctx.send(f'{user.mention}, your video conversion to mp4 is complete. Here is the converted video:', file=discord.File(video_path))
+
+            # Remove the file after 10 minutes
+            await asyncio.sleep(600)
+            os.remove(video_path)
 
         except Exception as e:
             error_message = str(e)
             await ctx.send(f"An error occurred during video conversion. Please check the URL and try again.\nError details: {error_message}")
-
-    async def download_and_convert(self, ctx, url, only_audio):
-        yt = YouTube(url)
-
-        if yt.age_restricted:
-            await ctx.send("This video is age-restricted and cannot be converted.")
-            return
-
-        stream = yt.streams.filter(only_audio=only_audio).first()
-
-        if not stream:
-            await ctx.send("Could not find a suitable stream for download.")
-            return
-
-        filename = f'{yt.video_id}.{"mp3" if only_audio else "mp4"}'
-
-        audio_path = os.path.join(self.data_path, filename)
-
-        async with aiohttp.ClientSession() as session:
-            async with session.get(stream.url) as response:
-                data = await response.read()
-
-        with open(audio_path, 'wb') as file:
-            file.write(data)
-
-        await asyncio.sleep(5)
-
-        user = ctx.author
-        await ctx.send(f'{user.mention}, your video conversion is complete. Here is the converted {"audio" if only_audio else "video"}:', file=discord.File(audio_path))
-
-        # Remove the file after 10 minutes
-        await asyncio.sleep(600)
-        os.remove(audio_path)
-
-def setup(bot):
-    bot.add_cog(ConverterCog(bot))
-import os
-import discord
-from redbot.core import commands
-from pytube import YouTube
-import aiohttp
-import asyncio
-import time
-
-class ConverterCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.data_path = self.bot.cog_data_path(self)
-
-    @commands.command()
-    async def ytmp3(self, ctx, url):
-        try:
-            await ctx.send("Converting the video to MP3, please wait...")
-            await self.download_and_convert(ctx, url, only_audio=True)
-
-        except Exception as e:
-            error_message = str(e)
-            await ctx.send(f"An error occurred during audio conversion. Please check the URL and try again.\nError details: {error_message}")
-
-    @commands.command()
-    async def ytmp4(self, ctx, url):
-        try:
-            await ctx.send("Converting the video to mp4, please wait...")
-            await self.download_and_convert(ctx, url, only_audio=False)
-
-        except Exception as e:
-            error_message = str(e)
-            await ctx.send(f"An error occurred during video conversion. Please check the URL and try again.\nError details: {error_message}")
-
-    async def download_and_convert(self, ctx, url, only_audio):
-        yt = YouTube(url)
-
-        if yt.age_restricted:
-            await ctx.send("This video is age-restricted and cannot be converted.")
-            return
-
-        stream = yt.streams.filter(only_audio=only_audio).first()
-
-        if not stream:
-            await ctx.send("Could not find a suitable stream for download.")
-            return
-
-        filename = f'{yt.video_id}.{"mp3" if only_audio else "mp4"}'
-
-        audio_path = os.path.join(self.data_path, filename)
-
-        async with aiohttp.ClientSession() as session:
-            async with session.get(stream.url) as response:
-                data = await response.read()
-
-        with open(audio_path, 'wb') as file:
-            file.write(data)
-
-        await asyncio.sleep(5)
-
-        user = ctx.author
-        await ctx.send(f'{user.mention}, your video conversion is complete. Here is the converted {"audio" if only_audio else "video"}:', file=discord.File(audio_path))
-
-        # Remove the file after 10 minutes
-        await asyncio.sleep(600)
-        os.remove(audio_path)
-
-def setup(bot):
-    bot.add_cog(ConverterCog(bot))
