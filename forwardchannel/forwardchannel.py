cf5232ee2c2de0e66ea547091004659f93c8ceb0
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-31 18:07:10 +0100

    Update forwardchannel.py

--- forwardchannel/forwardchannel.py
+++ forwardchannel/forwardchannel.py
@@ -1,5 +1,4 @@
 from redbot.core import commands, Config
-import discord  # Import the discord module
 
 class ForwardChannel(commands.Cog):
     def __init__(self, bot):
@@ -7,17 +6,11 @@ class ForwardChannel(commands.Cog):
         self.config = Config.get_conf(self, identifier=1234567890, force_registration=True)
         self.config.register_global(forwarding_rules=[])
 
-    @commands.command()
-    async def forward(self, ctx, action, *args):
-        if action == 'list':
-            await self.list_forwarding_rules(ctx)
-        elif action == 'remove':
-            await self.remove_forwarding_rule(ctx, args)
-        elif action == 'add':
-            await self.add_forwarding_rule(ctx, args)
-        else:
-            await ctx.send("Invalid action. Use 'list', 'remove', or 'add'.")
-
+    @commands.group()
+    async def forward(self, ctx):
+        """Manage forwarding rules."""
+    
+    @forward.command(name="list")
     async def list_forwarding_rules(self, ctx):
         forwarding_rules = await self.config.forwarding_rules()
         if not forwarding_rules:
@@ -26,43 +19,24 @@ class ForwardChannel(commands.Cog):
             rules = "\n".join([f"{rule['id']}: {rule['from_channel']} -> {rule['to_channel']}" for rule in forwarding_rules])
             await ctx.send(f"Forwarding Rules:\n{rules}")
 
-    async def remove_forwarding_rule(self, ctx, args):
+    @forward.command(name="remove")
+    async def remove_forwarding_rule(self, ctx, rule_id: int):
         forwarding_rules = await self.config.forwarding_rules()
-        if not args:
-            await ctx.send("Please provide the ID of the forwarding rule to remove.")
-            return
-
-        rule_id = args[0]
-        rule_id = int(rule_id) if rule_id.isdigit() else None
-
-        if rule_id is not None:
-            rule = next((r for r in forwarding_rules if r['id'] == rule_id), None)
-            if rule:
-                forwarding_rules.remove(rule)
-                await self.config.forwarding_rules.set(forwarding_rules)
-                await ctx.send(f"Removed forwarding rule {rule_id}: {rule['from_channel']} -> {rule['to_channel']}")
-            else:
-                await ctx.send(f"Forwarding rule with ID {rule_id} not found.")
-        else:
-            await ctx.send("Invalid rule ID.")
-
-    async def add_forwarding_rule(self, ctx, args):
-        if len(args) != 2:
-            await ctx.send("Usage: [p]forward add <channel to copy from> <channel to copy to>")
-            return
-
-        from_channel_name, to_channel_name = args
-        from_channel = discord.utils.get(ctx.guild.text_channels, name=from_channel_name)
-        to_channel = discord.utils.get(ctx.guild.text_channels, name=to_channel_name)
-
-        if from_channel and to_channel:
-            forwarding_rules = await self.config.forwarding_rules()
-            rule_id = len(forwarding_rules) + 1
-            forwarding_rules.append({'id': rule_id, 'from_channel': from_channel_name, 'to_channel': to_channel_name})
+        rule = next((r for r in forwarding_rules if r['id'] == rule_id), None)
+        if rule:
+            forwarding_rules.remove(rule)
             await self.config.forwarding_rules.set(forwarding_rules)
-            await ctx.send(f"Added forwarding rule {rule_id}: {from_channel_name} -> {to_channel_name}")
+            await ctx.send(f"Removed forwarding rule {rule_id}: {rule['from_channel']} -> {rule['to_channel']}")
         else:
-            await ctx.send("One or both of the specified channels do not exist")
+            await ctx.send(f"Forwarding rule with ID {rule_id} not found.")
+
+    @forward.command(name="add")
+    async def add_forwarding_rule(self, ctx, from_channel: str, to_channel: str):
+        forwarding_rules = await self.config.forwarding_rules()
+        rule_id = len(forwarding_rules) + 1
+        forwarding_rules.append({'id': rule_id, 'from_channel': from_channel, 'to_channel': to_channel})
+        await self.config.forwarding_rules.set(forwarding_rules)
+        await ctx.send(f"Added forwarding rule {rule_id}: {from_channel} -> {to_channel}")
 
 def setup(bot):
     bot.add_cog(ForwardChannel(bot))
