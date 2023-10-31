e06e3c590f729d757b0111b48984774d19383109
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-10-31 18:29:12 +0100

    Update __init__.py

--- forwardchannel/__init__.py
+++ forwardchannel/__init__.py
@@ -1,4 +1,6 @@
 from .forwardchannel import ForwardChannel
 
 async def setup(bot):
-    await bot.add_cog(ForwardChannel(bot))
+    cog = ForwardChannel(bot)
+    await bot.add_cog(cog)
+    await bot.add_listener(cog.forward_message, "on_message")
