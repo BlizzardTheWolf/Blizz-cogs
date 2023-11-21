abbec05dfb8b749aa79cb68b5eedf510f4354ca7
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-21 12:06:16 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -1,9 +1,11 @@
 import discord
 from redbot.core import commands
-import youtube_dlp
+from yt_dlp import YoutubeDL
 import asyncio
 import time
+import os
 from redbot.core import data_manager
+import shutil
 
 class ConverterCog(commands.Cog):
     def __init__(self, bot):
@@ -13,35 +15,36 @@ class ConverterCog(commands.Cog):
     async def download_and_convert(self, ctx, url, to_mp3=False):
         try:
             ydl_opts = {
-                'format': 'bestaudio' if to_mp3 else 'bestvideo+bestaudio/best',
-                'postprocessors': [{
-                    'key': 'FFmpegVideoConvertor',
-                    'preferedformat': 'mp4' if not to_mp3 else 'mp3',
-                }],
-                'outtmpl': str(self.data_folder / "downloads" / f'%(title)s.%(ext)s'),
-                'verbose': True,
+                'format': 'bestaudio/best' if to_mp3 else 'bestvideo+bestaudio/best',
+                'outtmpl': str(self.data_folder / "mp3" / "%(title)s.%(ext)s") if to_mp3 else str(self.data_folder / "mp4" / "%(title)s.%(ext)s"),
             }
 
-            with youtube_dlp.YoutubeDL(ydl_opts) as ydl:
+            with YoutubeDL(ydl_opts) as ydl:
                 info_dict = ydl.extract_info(url, download=False)
-                if 'entries' in info_dict:
-                    video = info_dict['entries'][0]
-                else:
-                    video = info_dict
 
-                if video.get('age_limit', 0) > 0:
+                if 'entries' in info_dict:
+                    video_info = info_dict['entries'][0]
+                else:
+                    video_info = info_dict
+
+                if video_info.get('age_limit') is not None:
                     await ctx.send("This video is age-restricted and cannot be converted.")
                     return
 
-                duration = video.get('duration', 0)
+                ydl.download([url])
 
-                if duration > 900:
-                    await ctx.send("Video exceeds the maximum time limit of 15 minutes.")
-                    return
+            await asyncio.sleep(5)
 
-                await ctx.send(f"Converting the video, please wait...")
+            user = ctx.message.author
+            output_folder = self.data_folder / ("mp3" if to_mp3 else "mp4")
+            file_path = output_folder / f"{video_info['title']}.{video_info['ext']}"
 
-                # Rest of your code remains unchanged...
+            await ctx.send(f'{user.mention}, your video conversion to {"MP3" if to_mp3 else "MP4"} is complete. Here is the converted file:',
+                           file=discord.File(str(file_path)))
+
+            # Remove the file after 10 minutes
+            await asyncio.sleep(600)
+            file_path.unlink()
 
         except Exception as e:
             error_message = str(e)
@@ -51,8 +54,9 @@ class ConverterCog(commands.Cog):
     async def ytmp3(self, ctx, url):
         """
         Converts a YouTube video to MP3.
+
         Parameters:
-        `<url>` The url of the video you want to convert.
+        `<url>` The URL of the video you want to convert.
         """
         await self.download_and_convert(ctx, url, to_mp3=True)
 
@@ -60,10 +64,8 @@ class ConverterCog(commands.Cog):
     async def ytmp4(self, ctx, url):
         """
         Converts a YouTube video to MP4.
-        **Parameters:**
-        `<url>` The url of the video you want to convert.
+
+        Parameters:
+        `<url>` The URL of the video you want to convert.
         """
         await self.download_and_convert(ctx, url, to_mp3=False)
-
-def setup(bot):
-    bot.add_cog(ConverterCog(bot))
