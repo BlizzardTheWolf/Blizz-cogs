33494b9286a57c90b958bd365e8effa9e993671e
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-21 12:27:58 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -2,45 +2,44 @@ import discord
 from redbot.core import commands
 from yt_dlp import YoutubeDL
 import asyncio
-import time
 import os
 from redbot.core import data_manager
+from discord.ext import tasks
 
 class ConverterCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.data_folder = data_manager.cog_data_path(cog_instance=self)
 
-    async def download_and_convert(self, ctx, url, to_mp3=False):
+    async def convert_and_send(self, ctx, url, to_mp3, output_folder):
         try:
-            async with ctx.typing():
-                ydl_opts = {
-                    'format': 'bestaudio/best' if to_mp3 else 'bestvideo+bestaudio/best',
-                    'outtmpl': str(self.data_folder / "mp3" / "%(title)s.%(ext)s") if to_mp3 else str(self.data_folder / "mp4" / "%(title)s.%(ext)s"),
-                }
+            ydl_opts = {
+                'format': 'bestaudio/best' if to_mp3 else 'bestvideo+bestaudio/best',
+                'outtmpl': str(output_folder / "%(title)s.%(ext)s"),
+            }
 
-                with YoutubeDL(ydl_opts) as ydl:
-                    info_dict = ydl.extract_info(url, download=False)
+            with YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(url, download=False)
 
-                    if 'entries' in info_dict:
-                        video_info = info_dict['entries'][0]
-                    else:
-                        video_info = info_dict
+                if 'entries' in info_dict:
+                    video_info = info_dict['entries'][0]
+                else:
+                    video_info = info_dict
 
-                    ydl.download([url])
+                ydl.download([url])
 
-                await asyncio.sleep(5)
+            user = ctx.message.author
+            file_path = output_folder / f"{video_info['title']}.{video_info['ext']}"
 
-                user = ctx.message.author
-                output_folder = self.data_folder / ("mp3" if to_mp3 else "mp4")
-                file_path = output_folder / f"{video_info['title']}.{video_info['ext']}"
+            await ctx.send(f'Please wait, your video is being converted to {"MP3" if to_mp3 else "MP4"}...')
+            await asyncio.sleep(5)  # Simulate conversion time
 
-                await ctx.send(f'{user.mention}, your video conversion to {"MP3" if to_mp3 else "MP4"} is complete. Here is the converted file:',
-                               file=discord.File(str(file_path)))
+            await ctx.send(f'{user.mention}, your video conversion to {"MP3" if to_mp3 else "MP4"} is complete. Here is the converted file:',
+                           file=discord.File(str(file_path)))
 
-                # Remove the file after 10 minutes
-                await asyncio.sleep(600)
-                file_path.unlink()
+            # Remove the file after 10 minutes
+            await asyncio.sleep(600)
+            file_path.unlink()
 
         except Exception as e:
             error_message = str(e)
@@ -54,7 +53,8 @@ class ConverterCog(commands.Cog):
         Parameters:
         `<url>` The URL of the video you want to convert.
         """
-        await self.download_and_convert(ctx, url, to_mp3=True)
+        output_folder = self.data_folder / "mp3"
+        self.bot.loop.create_task(self.convert_and_send(ctx, url, to_mp3=True, output_folder=output_folder))
 
     @commands.command()
     async def ytmp4(self, ctx, url):
@@ -64,4 +64,5 @@ class ConverterCog(commands.Cog):
         Parameters:
         `<url>` The URL of the video you want to convert.
         """
-        await self.download_and_convert(ctx, url, to_mp3=False)
+        output_folder = self.data_folder / "mp4"
+        self.bot.loop.create_task(self.convert_and_send(ctx, url, to_mp3=False, output_folder=output_folder))
