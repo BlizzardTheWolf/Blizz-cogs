eaf14e846c60ded73db220db52ba1e49557b0ae2
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-21 12:45:04 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -4,6 +4,7 @@ from yt_dlp import YoutubeDL
 import asyncio
 import os
 from redbot.core import data_manager
+import aiofiles
 
 class ConverterCog(commands.Cog):
     def __init__(self, bot):
@@ -17,32 +18,36 @@ class ConverterCog(commands.Cog):
                 'outtmpl': str(output_folder / "%(title)s.%(ext)s"),
             }
 
-            with YoutubeDL(ydl_opts) as ydl:
-                info_dict = ydl.extract_info(url, download=False)
+            async with ctx.typing():  # Show that the bot is typing
+                # Use run_in_executor to run synchronous code in a separate thread
+                loop = asyncio.get_event_loop()
+                info_dict = await loop.run_in_executor(None, lambda: self.download_video(ydl_opts, url))
 
-                if 'entries' in info_dict:
-                    video_info = info_dict['entries'][0]
-                else:
-                    video_info = info_dict
+            if 'entries' in info_dict:
+                video_info = info_dict['entries'][0]
+            else:
+                video_info = info_dict
 
-                ydl.download([url])
-
-            user = ctx.message.author
             file_path = output_folder / f"{video_info['title']}.{video_info['ext']}"
 
-            async with ctx.typing():
-                await asyncio.sleep(5)  # Simulate conversion time
+            await ctx.send("Please wait, the video is being converted...")
 
-            await ctx.send(f'{user.mention}, your video conversion to {"MP3" if to_mp3 else "MP4"} is complete. Here is the converted file:',
+            await asyncio.sleep(5)  # Simulate conversion time
+
+            await ctx.send(f'{ctx.author.mention}, your video conversion to {"MP3" if to_mp3 else "MP4"} is complete. Here is the converted file:',
                            file=discord.File(str(file_path)))
 
             # Remove the file after 10 minutes
             await asyncio.sleep(600)
-            file_path.unlink()
+            await loop.run_in_executor(None, file_path.unlink)
 
         except Exception as e:
             error_message = str(e)
             await ctx.send(f"An error occurred during conversion. Please check the URL and try again.\nError details: {error_message}")
+
+    def download_video(self, ydl_opts, url):
+        with YoutubeDL(ydl_opts) as ydl:
+            return ydl.extract_info(url, download=False)
 
     @commands.command()
     async def ytmp3(self, ctx, url):
@@ -53,8 +58,7 @@ class ConverterCog(commands.Cog):
         `<url>` The URL of the video you want to convert.
         """
         output_folder = self.data_folder / "mp3"
-        await ctx.trigger_typing()  # Trigger typing to show the "Thinking..." status
-        self.bot.loop.create_task(self.convert_and_send(ctx, url, to_mp3=True, output_folder=output_folder))
+        await self.convert_and_send(ctx, url, to_mp3=True, output_folder=output_folder)
 
     @commands.command()
     async def ytmp4(self, ctx, url):
@@ -65,5 +69,4 @@ class ConverterCog(commands.Cog):
         `<url>` The URL of the video you want to convert.
         """
         output_folder = self.data_folder / "mp4"
-        await ctx.trigger_typing()  # Trigger typing to show the "Thinking..." status
-        self.bot.loop.create_task(self.convert_and_send(ctx, url, to_mp3=False, output_folder=output_folder))
+        await self.convert_and_send(ctx, url, to_mp3=False, output_folder=output_folder)
