da95dc186040e9fff8360c8540d08db3067eabd6
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-21 18:25:11 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -19,38 +19,40 @@ class ConverterCog(commands.Cog):
                 'outtmpl': str(output_folder / f"%(id)s.{'mp3' if to_mp3 else 'webm'}"),
             }
 
-            async with ctx.typing(ephemeral=False):  # Start typing indication
-                await ctx.send("Please wait, the video is being converted...")
+            await ctx.trigger_typing()  # Start typing indication
 
-                with YoutubeDL(ydl_opts) as ydl:
-                    info_dict = ydl.extract_info(url, download=False)
+            with YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(url, download=False)
 
-                    if 'entries' in info_dict:
-                        video_info = info_dict['entries'][0]
-                    else:
-                        video_info = info_dict
+                if 'entries' in info_dict:
+                    video_info = info_dict['entries'][0]
+                else:
+                    video_info = info_dict
 
-                    ydl.download([url])
+                ydl.download([url])
 
-                await asyncio.sleep(5)
+            await asyncio.sleep(5)
 
-                user = ctx.message.author
-                downloaded_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'webm'}"
-                renamed_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'mp4'}"
+            user = ctx.message.author
+            downloaded_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'webm'}"
+            renamed_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'mp4'}"
 
-                downloaded_file_path.rename(renamed_file_path)
+            downloaded_file_path.rename(renamed_file_path)
 
-                await ctx.send(f'{user.mention}, your video conversion to {"MP3" if to_mp3 else "MP4"} is complete. Here is the converted file:',
-                            file=discord.File(str(renamed_file_path)))
+            await ctx.send(f'{user.mention}, your video conversion to {"MP3" if to_mp3 else "MP4"} is complete. Here is the converted file:',
+                           file=discord.File(str(renamed_file_path)))
 
-                # Remove the file after 10 minutes if it exists
-                await asyncio.sleep(600)
-                if renamed_file_path.exists():
-                    renamed_file_path.unlink()
+            # Remove the file after 10 minutes if it exists
+            await asyncio.sleep(600)
+            if renamed_file_path.exists():
+                renamed_file_path.unlink()
 
         except Exception as e:
             error_message = str(e)
             await ctx.send(f"An error occurred during conversion. Please check the URL and try again.\nError details: {error_message}")
+
+        finally:
+            await ctx.trigger_typing(False)  # Stop typing indication
 
     @commands.command()
     async def ytmp3(self, ctx, url):
