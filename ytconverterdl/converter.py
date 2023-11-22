3d2c9c16102850bc4c8469dbc3cfdac169c3872e
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-22 17:01:32 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -17,8 +17,8 @@ class ConverterCog(commands.Cog):
             # Load the video clip
             video_clip = VideoFileClip(str(input_path))
 
-            # Convert target_size_bytes to float
-            target_size_bytes = float(target_size_bytes)
+            # Convert target_size_bytes to int
+            target_size_bytes = int(target_size_bytes)
 
             # Calculate the bitrate to achieve the target size
             target_bitrate = int(target_size_bytes * 8 / video_clip.duration)
@@ -66,7 +66,7 @@ class ConverterCog(commands.Cog):
 
             file_size = renamed_file_path.stat().st_size
 
-            if max_size_mb is not None and file_size > max_size_mb * 1024 * 1024:
+            if max_size_mb is not None and file_size > int(max_size_mb) * 1024 * 1024:
                 await conversion_message.edit(content=f"`Transcoding to your specified size...`")
                 # Resize the video to meet the size requirement
                 await self.resize_video(renamed_file_path, renamed_file_path, max_size_mb * 1024 * 1024)
