f65c1e22dd7ab2359a43c0423e6c681919dbb7c3
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-22 10:47:26 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -10,16 +10,16 @@ class ConverterCog(commands.Cog):
         self.bot = bot
         self.data_folder = data_manager.cog_data_path(cog_instance=self)
 
-    async def download_and_convert(self, ctx, url, to_mp3=False):
+    async def download_and_convert(self, ctx, url, max_size_mb=8):
         try:
-            output_folder = self.data_folder / ("mp3" if to_mp3 else "mp4")
+            output_folder = self.data_folder / "mp4"
 
             ydl_opts = {
-                'format': 'bestaudio/best' if to_mp3 else 'bestvideo[ext=mp4]+bestaudio/best',
-                'outtmpl': str(output_folder / f"%(id)s.{'mp3' if to_mp3 else 'webm'}"),
+                'format': 'bestvideo[ext=mp4]+bestaudio/best',
+                'outtmpl': str(output_folder / f"%(id)s.mp4"),
             }
 
-            await ctx.send(f"`Your video is being converted...`")  # Notify that the video is being converted
+            conversion_message = await ctx.send(f"`Your video is being converted...`")  # Notify that the video is being converted
 
             with YoutubeDL(ydl_opts) as ydl:
                 info_dict = ydl.extract_info(url, download=False)
@@ -34,47 +34,35 @@ class ConverterCog(commands.Cog):
             await asyncio.sleep(5)
 
             user = ctx.message.author
-            downloaded_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'webm'}"
-            renamed_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'mp4'}"
+            downloaded_file_path = output_folder / f"{video_info['id']}.mp4"
 
-            downloaded_file_path.rename(renamed_file_path)
+            file_size = downloaded_file_path.stat().st_size  # Get file size in bytes
 
-            file_size = renamed_file_path.stat().st_size  # Get file size in bytes
-
-            if file_size <= 8000000:  # Check if file size is less than or equal to 8 MB (Discord limit)
-                await ctx.send(f"`Your video conversion to {'MP3' if to_mp3 else 'MP4'} is complete. Uploading...`")
+            if file_size <= max_size_mb * 1024 * 1024:  # Check if file size is less than or equal to the specified limit
+                await conversion_message.edit(content=f"`Your video conversion to MP4 is complete. Uploading...`")
                 # Send a new message with the converted file
                 await ctx.send(f'`Here is the converted file:`',
-                               file=discord.File(str(renamed_file_path)))
+                               file=discord.File(str(downloaded_file_path)))
             else:
                 # If the file size exceeds the limit, inform the user about the size
                 await ctx.send(f"`The converted file is too large to send ({file_size / (1024 * 1024):.2f} MB). "
-                               f"Discord has a file size limit of 8 MB for regular users. "
+                               f"Discord has a file size limit of {max_size_mb} MB for regular users. "
                                f"If you need to send larger files, consider boosting the server for a higher limit.`")
                 # Remove the file after 10 minutes if it exists
-                if renamed_file_path.exists():
-                    renamed_file_path.unlink()
+                if downloaded_file_path.exists():
+                    downloaded_file_path.unlink()
 
         except Exception as e:
             error_message = str(e)
             await ctx.send(f"`An error occurred during conversion. Please check the URL and try again.\nError details: {error_message}`")
 
     @commands.command()
-    async def ytmp3(self, ctx, url):
-        """
-        Converts a YouTube video to MP3.
-
-        Parameters:
-        `<url>` The URL of the video you want to convert.
-        """
-        await self.download_and_convert(ctx, url, to_mp3=True)
-
-    @commands.command()
-    async def ytmp4(self, ctx, url):
+    async def ytmp4(self, ctx, url, max_size_mb=8):
         """
         Converts a YouTube video to MP4.
 
         Parameters:
         `<url>` The URL of the video you want to convert.
+        `[max_size_mb]` The maximum allowed file size in MB. Default is 8 MB.
         """
-        await self.download_and_convert(ctx, url, to_mp3=False)
+        await self.download_and_convert(ctx, url, max_size_mb=max_size_mb)
