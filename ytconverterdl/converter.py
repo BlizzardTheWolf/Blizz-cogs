238576afef2bdd23f63a54477aac6354ac14f3ac
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-22 13:09:30 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -16,19 +16,20 @@ class ConverterCog(commands.Cog):
             # Load the video clip
             video_clip = VideoFileClip(str(input_path))
 
-            # Resize the video to meet the size requirement
+            # Calculate the bitrate to achieve the target size
+            target_bitrate = int(target_size_bytes * 8 / video_clip.duration)
+
+            # Resize the video and adjust the bitrate
             resized_clip = video_clip.resize(height=720)
+            resized_clip = resized_clip.set_audio(resized_clip.audio.set_bitrate(str(target_bitrate) + 'bit'))
 
             # Write the resized video to the output path
             resized_clip.write_videofile(str(output_path), codec="libx264", audio_codec="aac", temp_audiofile="temp-audio.m4a", remove_temp=True, threads=4)
 
-            # Check if the resized file size is within the limit
-            if output_path.stat().st_size > target_size_bytes:
-                raise ValueError(f"File size exceeds the limit ({target_size_bytes / (1024 * 1024):.2f} MB).")
         except Exception as e:
             raise ValueError(f"Error during video resizing: {e}")
 
-    async def download_and_convert(self, ctx, url, to_mp3=False, max_size_mb=8):
+    async def download_and_convert(self, ctx, url, to_mp3=False, max_size_mb=None):
         try:
             output_folder = self.data_folder / ("mp3" if to_mp3 else "mp4")
 
@@ -59,21 +60,19 @@ class ConverterCog(commands.Cog):
 
             file_size = renamed_file_path.stat().st_size
 
-            if file_size <= max_size_mb * 1024 * 1024:
-                await conversion_message.edit(content=f"`Uploading...`")
+            if max_size_mb is not None and file_size > max_size_mb * 1024 * 1024:
+                await conversion_message.edit(content=f"`Transcoding to your specified size...`")
                 # Resize the video to meet the size requirement
                 await self.resize_video(renamed_file_path, renamed_file_path, max_size_mb * 1024 * 1024)
-                # Send a new message with the converted file
-                await ctx.send(f'`Done`', file=discord.File(str(renamed_file_path)))
-            else:
-                # If the file size exceeds the limit, inform the user about the size
-                await ctx.send(f"`The converted file is too large to send ({file_size / (1024 * 1024):.2f} MB). "
-                               f"Discord has a file size limit of 8 MB for regular users. "
-                               f"If you need to send larger files, consider boosting the server for a higher limit.`")
-                # Remove the file after 1 minute if it exists
-                await asyncio.sleep(60)
-                if renamed_file_path.exists():
-                    renamed_file_path.unlink()
+
+            await conversion_message.edit(content=f"`Uploading...`")
+            # Send a new message with the converted file
+            await ctx.send(f'`Done`', file=discord.File(str(renamed_file_path)))
+
+            # Remove the file after 1 minute if it exists
+            await asyncio.sleep(60)
+            if renamed_file_path.exists():
+                renamed_file_path.unlink()
 
         except Exception as e:
             error_message = str(e)
@@ -90,12 +89,12 @@ class ConverterCog(commands.Cog):
         await self.download_and_convert(ctx, url, to_mp3=True)
 
     @commands.command()
-    async def ytmp4(self, ctx, url, max_size_mb=8):
+    async def ytmp4(self, ctx, url, max_size_mb=None):
         """
         Converts a YouTube video to MP4.
 
         Parameters:
         `<url>` The URL of the video you want to convert.
-        `<max_size_mb>` The maximum size in megabytes for the converted video.
+        `[max_size_mb]` The maximum size in megabytes for the converted video (optional).
         """
         await self.download_and_convert(ctx, url, to_mp3=False, max_size_mb=max_size_mb)
