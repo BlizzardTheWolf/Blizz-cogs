5c85543f1c4f33d64a96a9a0134fe074f99cdb8b
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-22 20:55:29 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -11,17 +11,13 @@ class ConverterCog(commands.Cog):
         self.bot = bot
         self.data_folder = data_manager.cog_data_path(cog_instance=self)
 
-    async def resize_video(self, input_path, output_path, target_size_bytes):
+    async def resize_video(self, input_path, output_path):
         try:
             # Load the video clip
             video_clip = VideoFileClip(str(input_path))
 
-            # Calculate the bitrate to achieve the target size
-            target_bitrate = int(target_size_bytes * 8 / video_clip.duration)
-
-            # Resize the video and adjust the bitrate
+            # Resize the video to 720p
             resized_clip = video_clip.resize(height=720)
-            resized_clip = resized_clip.set_audio(resized_clip.audio.set_audio_params(bitrate=f"{target_bitrate}bit"))
 
             # Write the resized video to the output path
             resized_clip.write_videofile(str(output_path), codec="libx264", audio_codec="aac", temp_audiofile="temp-audio.m4a", remove_temp=True, threads=4)
@@ -29,7 +25,7 @@ class ConverterCog(commands.Cog):
         except Exception as e:
             raise ValueError(f"Error during video resizing: {e}")
 
-    async def download_and_convert(self, ctx, url, to_mp3=False, max_size_mb=None):
+    async def download_and_convert(self, ctx, url, to_mp3=False):
         try:
             output_folder = self.data_folder / ("mp3" if to_mp3 else "mp4")
 
@@ -38,7 +34,7 @@ class ConverterCog(commands.Cog):
                 'outtmpl': str(output_folder / f"%(id)s.{'mp3' if to_mp3 else 'webm'}"),
             }
 
-            conversion_message = await ctx.send(f"`Converting...`")
+            conversion_message = await ctx.send(f"`Downloading...`")
 
             with YoutubeDL(ydl_opts) as ydl:
                 info_dict = ydl.extract_info(url, download=False)
@@ -58,12 +54,9 @@ class ConverterCog(commands.Cog):
 
             downloaded_file_path.rename(renamed_file_path)
 
-            file_size = renamed_file_path.stat().st_size
-
-            if max_size_mb is not None and file_size > max_size_mb * 1024 * 1024:
-                await conversion_message.edit(content=f"`Transcoding to your specified size...`")
-                # Resize the video to meet the size requirement
-                await self.resize_video(renamed_file_path, renamed_file_path, max_size_mb * 1024 * 1024)
+            await conversion_message.edit(content=f"`Transcoding to 720p...`")
+            # Resize the video to 720p
+            await self.resize_video(renamed_file_path, renamed_file_path)
 
             await conversion_message.edit(content=f"`Uploading...`")
             # Send a new message with the converted file
@@ -89,12 +82,11 @@ class ConverterCog(commands.Cog):
         await self.download_and_convert(ctx, url, to_mp3=True)
 
     @commands.command()
-    async def ytmp4(self, ctx, url, max_size_mb=None):
+    async def ytmp4(self, ctx, url):
         """
         Converts a YouTube video to MP4.
 
         Parameters:
         `<url>` The URL of the video you want to convert.
-        `[max_size_mb]` The maximum size in megabytes for the converted video (optional).
         """
-        await self.download_and_convert(ctx, url, to_mp3=False, max_size_mb=max_size_mb)
+        await self.download_and_convert(ctx, url, to_mp3=False)
