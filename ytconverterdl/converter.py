f1962a50bc6b1be44e0ed6352d20766d5b149d61
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-22 21:40:48 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -4,41 +4,32 @@ from yt_dlp import YoutubeDL
 import asyncio
 from redbot.core import data_manager
 from pathlib import Path
-from moviepy.editor import VideoFileClip
+from flask import Flask, send_file
+import threading
+
+app = Flask(__name__)
 
 class ConverterCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.data_folder = data_manager.cog_data_path(cog_instance=self)
+        self.video_path = "videos/"
+        self.server_thread = threading.Thread(target=app.run, kwargs={"port": 4090})
+        self.server_thread.start()
 
-    async def resize_video(self, input_path, output_path, target_size_bytes):
+    def cog_unload(self):
+        # Stop the Flask server thread when the cog is unloaded
+        self.server_thread.join()
+
+    @commands.command()
+    async def ytmp4(self, ctx, url):
         try:
-            # Load the video clip
-            video_clip = VideoFileClip(str(input_path))
-
-            # Calculate the bitrate to achieve the target size
-            target_bitrate = int(target_size_bytes * 8 / video_clip.duration)
-
-            # Resize the video and adjust the bitrate
-            resized_clip = video_clip.resize(height=720)
-            resized_clip = resized_clip.set_audio(resized_clip.audio.set_audio_params(bitrate=f"{target_bitrate}bit"))
-
-            # Write the resized video to the output path
-            resized_clip.write_videofile(str(output_path), codec="libx264", audio_codec="aac", temp_audiofile="temp-audio.m4a", remove_temp=True, threads=4)
-
-        except Exception as e:
-            raise ValueError(f"Error during video resizing: {e}")
-
-    async def download_and_convert(self, ctx, url, to_mp3=False, max_size_mb=None):
-        try:
-            output_folder = self.data_folder / ("mp3" if to_mp3 else "mp4")
-
+            output_folder = self.video_path
             ydl_opts = {
-                'format': 'bestaudio/best' if to_mp3 else 'bestvideo[ext=mp4]+bestaudio/best',
-                'outtmpl': str(output_folder / f"%(id)s.{'mp3' if to_mp3 else 'webm'}"),
+                'format': 'bestvideo[ext=mp4]+bestaudio/best',
+                'outtmpl': str(output_folder / "%(id)s.{'mp4'}"),
             }
 
-            conversion_message = await ctx.send(f"`Converting...`")
+            conversion_message = await ctx.send("`Video is being processed...`")
 
             with YoutubeDL(ydl_opts) as ydl:
                 info_dict = ydl.extract_info(url, download=False)
@@ -52,49 +43,21 @@ class ConverterCog(commands.Cog):
 
             await asyncio.sleep(5)
 
-            user = ctx.message.author
-            downloaded_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'webm'}"
-            renamed_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'mp4'}"
+            video_id = video_info['id']
+            video_filename = f"{video_id}.mp4"
+            video_path = Path(self.video_path) / video_filename
 
-            downloaded_file_path.rename(renamed_file_path)
+            await ctx.send(f"Here is your video: http://web.purplepanda.cc:4090/{video_filename}")
 
-            file_size = renamed_file_path.stat().st_size
-
-            if max_size_mb is not None and file_size > max_size_mb * 1024 * 1024:
-                await conversion_message.edit(content=f"`Transcoding to your specified size...`")
-                # Resize the video to meet the size requirement
-                await self.resize_video(renamed_file_path, renamed_file_path, max_size_mb * 1024 * 1024)
-
-            await conversion_message.edit(content=f"`Uploading...`")
-            # Send a new message with the converted file
-            await ctx.send(f'`Done`', file=discord.File(str(renamed_file_path)))
-
-            # Remove the file after 1 minute if it exists
-            await asyncio.sleep(60)
-            if renamed_file_path.exists():
-                renamed_file_path.unlink()
+            # Remove the file after 10 minutes
+            await asyncio.sleep(600)
+            try:
+                # Remove the file if it exists
+                if video_path.exists():
+                    video_path.unlink()
+            except Exception as e:
+                print(f"Error removing video file: {e}")
 
         except Exception as e:
             error_message = str(e)
-            await ctx.send(f"`An error occurred during conversion. Please check the URL and try again.\nError details: {error_message}`")
-
-    @commands.command()
-    async def ytmp3(self, ctx, url):
-        """
-        Converts a YouTube video to MP3.
-
-        Parameters:
-        `<url>` The URL of the video you want to convert.
-        """
-        await self.download_and_convert(ctx, url, to_mp3=True)
-
-    @commands.command()
-    async def ytmp4(self, ctx, url, max_size_mb=None):
-        """
-        Converts a YouTube video to MP4.
-
-        Parameters:
-        `<url>` The URL of the video you want to convert.
-        `[max_size_mb]` The maximum size in megabytes for the converted video (optional).
-        """
-        await self.download_and_convert(ctx, url, to_mp3=False, max_size_mb=max_size_mb)
+            await ctx.send(f"`An error occurred during video processing. Please check the URL and try again.\nError details: {error_message}`")
