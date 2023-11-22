fb0d83beb54c818c774b138615fde420aca29171
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-22 21:28:16 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -4,60 +4,79 @@ from yt_dlp import YoutubeDL
 import asyncio
 from redbot.core import data_manager
 from pathlib import Path
+from moviepy.editor import VideoFileClip
 
 class ConverterCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.data_folder = data_manager.cog_data_path(cog_instance=self)
 
-    async def download_and_convert(self, ctx, url, to_mp3=False):
+    async def resize_video(self, input_path, output_path, target_size_bytes):
+        try:
+            # Load the video clip
+            video_clip = VideoFileClip(str(input_path))
+
+            # Calculate the bitrate to achieve the target size
+            target_bitrate = int(target_size_bytes * 8 / video_clip.duration)
+
+            # Resize the video and adjust the bitrate
+            resized_clip = video_clip.resize(height=720)
+            resized_clip = resized_clip.set_audio(resized_clip.audio.set_audio_params(bitrate=f"{target_bitrate}bit"))
+
+            # Write the resized video to the output path
+            resized_clip.write_videofile(str(output_path), codec="libx264", audio_codec="aac", temp_audiofile="temp-audio.m4a", remove_temp=True, threads=4)
+
+        except Exception as e:
+            raise ValueError(f"Error during video resizing: {e}")
+
+    async def download_and_convert(self, ctx, url, to_mp3=False, max_size_mb=None):
         try:
             output_folder = self.data_folder / ("mp3" if to_mp3 else "mp4")
 
             ydl_opts = {
-                'format': 'bestvideo[height<=720]+bestaudio/best' if not to_mp3 else 'bestaudio/best',
+                'format': 'bestaudio/best' if to_mp3 else 'bestvideo[ext=mp4]+bestaudio/best',
                 'outtmpl': str(output_folder / f"%(id)s.{'mp3' if to_mp3 else 'webm'}"),
             }
 
-            conversion_message = await ctx.send(f"`Downloading...`")
+            conversion_message = await ctx.send(f"`Converting...`")
 
             with YoutubeDL(ydl_opts) as ydl:
                 info_dict = ydl.extract_info(url, download=False)
 
                 if 'entries' in info_dict:
-                    formats = info_dict['entries'][0].get('formats', [])
+                    video_info = info_dict['entries'][0]
+                else:
+                    video_info = info_dict
 
-                    if formats:
-                        # Sort by quality
-                        formats = sorted(formats, key=lambda x: x.get('height', float('inf')), reverse=True)
+                ydl.download([url])
 
-                        # Find the first format with video
-                        video_format = next((f for f in formats if 'height' in f), None)
+            await asyncio.sleep(5)
 
-                        if video_format:
-                            ydl_opts['format'] = f"{video_format['format_id']}/bestaudio/best" if not to_mp3 else 'bestaudio/best'
-                            ydl_opts['outtmpl'] = str(output_folder / f"%(id)s.{'mp3' if to_mp3 else 'webm'}")
+            user = ctx.message.author
+            downloaded_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'webm'}"
+            renamed_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'mp4'}"
 
-                            ydl.download([url])
+            downloaded_file_path.rename(renamed_file_path)
 
-                            await conversion_message.edit(content=f"`Uploading...`")
-                            # Send a new message with the converted file
-                            file_path = output_folder / f"{info_dict['entries'][0]['id']}.{'mp3' if to_mp3 else 'webm'}"
-                            file_size = file_path.stat().st_size
-                            await ctx.send(f"{ctx.author.mention} `Done | Size: {file_size / (1024 * 1024):.2f} MB`", file=discord.File(str(file_path)))
+            file_size = renamed_file_path.stat().st_size
 
-                            # Remove the file after 1 minute if it exists
-                            await asyncio.sleep(60)
-                            if file_path.exists():
-                                file_path.unlink()
-                            return
+            if max_size_mb is not None and file_size > max_size_mb * 1024 * 1024:
+                await conversion_message.edit(content=f"`Transcoding to your specified size...`")
+                # Resize the video to meet the size requirement
+                await self.resize_video(renamed_file_path, renamed_file_path, max_size_mb * 1024 * 1024)
 
-            # If no suitable format found
-            raise ValueError("No suitable format available for download.")
+            await conversion_message.edit(content=f"`Uploading...`")
+            # Send a new message with the converted file
+            await ctx.send(f'`Done`', file=discord.File(str(renamed_file_path)))
+
+            # Remove the file after 1 minute if it exists
+            await asyncio.sleep(60)
+            if renamed_file_path.exists():
+                renamed_file_path.unlink()
 
         except Exception as e:
             error_message = str(e)
-            await ctx.send(f"{ctx.author.mention} `An error occurred during conversion. Please check the URL and try again.\nError details: {error_message}`")
+            await ctx.send(f"`An error occurred during conversion. Please check the URL and try again.\nError details: {error_message}`")
 
     @commands.command()
     async def ytmp3(self, ctx, url):
@@ -70,11 +89,12 @@ class ConverterCog(commands.Cog):
         await self.download_and_convert(ctx, url, to_mp3=True)
 
     @commands.command()
-    async def ytmp4(self, ctx, url):
+    async def ytmp4(self, ctx, url, max_size_mb=None):
         """
         Converts a YouTube video to MP4.
 
         Parameters:
         `<url>` The URL of the video you want to convert.
+        `[max_size_mb]` The maximum size in megabytes for the converted video (optional).
         """
-        await self.download_and_convert(ctx, url, to_mp3=False)
+        await self.download_and_convert(ctx, url, to_mp3=False, max_size_mb=max_size_mb)
