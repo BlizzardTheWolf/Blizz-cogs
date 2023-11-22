8a253070986cb19a79e65fda0fd6ef047dd936f7
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-22 12:04:12 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -11,7 +11,18 @@ class ConverterCog(commands.Cog):
         self.bot = bot
         self.data_folder = data_manager.cog_data_path(cog_instance=self)
 
-    async def download_and_convert(self, ctx, url, to_mp3=False, max_size_mb=None):
+    async def transcode_video(self, input_path, output_path, max_size_bytes):
+        try:
+            # Use FFmpeg to transcode the video to the required size
+            subprocess.run(['ffmpeg', '-i', str(input_path), '-b:v', '500k', '-maxrate', '500k', '-bufsize', '1000k', '-c:a', 'copy', str(output_path)], check=True)
+
+            # Check if the transcoded file size is within the limit
+            if output_path.stat().st_size > max_size_bytes:
+                raise ValueError(f"File size exceeds the limit ({max_size_bytes / (1024 * 1024):.2f} MB).")
+        except subprocess.CalledProcessError as e:
+            raise ValueError(f"Error during video transcoding: {e}")
+
+    async def download_and_convert(self, ctx, url, to_mp3=False, max_size_mb=8):
         try:
             output_folder = self.data_folder / ("mp3" if to_mp3 else "mp4")
 
@@ -40,36 +51,27 @@ class ConverterCog(commands.Cog):
 
             downloaded_file_path.rename(renamed_file_path)
 
-            transcoding_message = await conversion_message.edit(content=f"`Transcoding to required size...`")
+            file_size = renamed_file_path.stat().st_size
 
-            # Use FFmpeg to limit the file size
-            if max_size_mb:
-                max_size_bytes = int(max_size_mb) * 1024 * 1024
-                await self.transcode_video(renamed_file_path, renamed_file_path, max_size_bytes)
-
-            # Send a new message with the converted file
-            await ctx.send(f'`Done.`',
-                           file=discord.File(str(renamed_file_path)))
-
-            # Remove the file after 1 minute if it exists
-            await asyncio.sleep(60)
-            if renamed_file_path.exists():
-                renamed_file_path.unlink()
+            if file_size <= 8000000:
+                await conversion_message.edit(content=f"`Uploading...`")
+                # Transcode the video to meet the size requirement
+                await self.transcode_video(renamed_file_path, renamed_file_path, max_size_mb * 1024 * 1024)
+                # Send a new message with the converted file
+                await ctx.send(f'`Done`', file=discord.File(str(renamed_file_path)))
+            else:
+                # If the file size exceeds the limit, inform the user about the size
+                await ctx.send(f"`The converted file is too large to send ({file_size / (1024 * 1024):.2f} MB). "
+                               f"Discord has a file size limit of 8 MB for regular users. "
+                               f"If you need to send larger files, consider boosting the server for a higher limit.`")
+                # Remove the file after 1 minute if it exists
+                await asyncio.sleep(60)
+                if renamed_file_path.exists():
+                    renamed_file_path.unlink()
 
         except Exception as e:
             error_message = str(e)
             await ctx.send(f"`An error occurred during conversion. Please check the URL and try again.\nError details: {error_message}`")
-
-    async def transcode_video(self, input_path, output_path, max_size_bytes):
-        try:
-            # Use FFmpeg to transcode the video to the required size
-            subprocess.run(['ffmpeg', '-i', str(input_path), '-b', '500k', '-maxrate', '500k', '-bufsize', '1000k', str(output_path)], check=True)
-
-            # Check if the transcoded file size is within the limit
-            if output_path.stat().st_size > max_size_bytes:
-                raise ValueError(f"File size exceeds the limit ({max_size_bytes / (1024 * 1024):.2f} MB).")
-        except subprocess.CalledProcessError as e:
-            raise ValueError(f"Error during video transcoding: {e}")
 
     @commands.command()
     async def ytmp3(self, ctx, url):
@@ -82,12 +84,12 @@ class ConverterCog(commands.Cog):
         await self.download_and_convert(ctx, url, to_mp3=True)
 
     @commands.command()
-    async def ytmp4(self, ctx, url, max_size_mb=None):
+    async def ytmp4(self, ctx, url, max_size_mb=8):
         """
         Converts a YouTube video to MP4.
 
         Parameters:
         `<url>` The URL of the video you want to convert.
-        `[max_size_mb]` Maximum file size in megabytes.
+        `<max_size_mb>` The maximum size in megabytes for the converted video.
         """
         await self.download_and_convert(ctx, url, to_mp3=False, max_size_mb=max_size_mb)
