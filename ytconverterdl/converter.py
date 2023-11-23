fdcaf17cea921c3788c7dad68d9c1fbe3182f025
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-23 14:18:51 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -11,8 +11,8 @@ class ConverterCog(commands.Cog):
         self.data_folder = data_manager.cog_data_path(cog_instance=self)
         self.app = aiohttp.web.Application()
         self.runner = aiohttp.web.AppRunner(self.app)
-        self.hostname = "127.0.0.1"  # Set a default hostname
-        self.port = 8080
+        self.hostname = "web.purplepanda.cc"
+        self.port = 4090
         self.is_server_running = False
         self.start_server_task = None
 
@@ -37,12 +37,14 @@ class ConverterCog(commands.Cog):
         with open(video_path, 'rb') as f:
             return aiohttp.web.Response(body=f.read(), content_type='audio/mpeg' if video_path.suffix == '.mp3' else 'video/mp4')
 
-    async def download_and_convert(self, url, format):
+    async def download_and_convert(self, ctx, url, format):
         ydl_opts = {
             'format': 'bestaudio/best' if format == 'mp3' else 'bestvideo+bestaudio/best',
             'outtmpl': f'{self.data_folder}/%(title)s.%(ext)s',
         }
 
+        await ctx.send(f"Converting to {format}...")
+        
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(url, download=True)
             filename = ydl.prepare_filename(info_dict)
@@ -51,13 +53,13 @@ class ConverterCog(commands.Cog):
 
     @commands.command()
     async def ytmp3(self, ctx, url):
-        filename = await self.download_and_convert(url, 'mp3')
-        await ctx.send(f"Conversion complete! You can download the MP3 [here]({await self.get_download_url(filename)})")
+        filename = await self.download_and_convert(ctx, url, 'mp3')
+        await ctx.send(f"Conversion complete! You can download the MP3 [here]({await self.get_download_url(filename)}) {ctx.author.mention}")
 
     @commands.command()
     async def ytmp4(self, ctx, url):
-        filename = await self.download_and_convert(url, 'mp4')
-        await ctx.send(f"Conversion complete! You can download the MP4 [here]({await self.get_download_url(filename)})")
+        filename = await self.download_and_convert(ctx, url, 'mp4')
+        await ctx.send(f"Conversion complete! You can download the MP4 [here]({await self.get_download_url(filename)}) {ctx.author.mention}")
 
     @commands.command()
     @commands.is_owner()
@@ -70,7 +72,7 @@ class ConverterCog(commands.Cog):
     async def ytcurl(self, ctx, filename):
         if self.is_server_running:
             download_url = await self.get_download_url(filename)
-            await ctx.send(f"Download URL for {filename}: {download_url}")
+            await ctx.send(f"Download URL for {filename}: {download_url} {ctx.author.mention}")
         else:
             await ctx.send("Web server is not running. Start the server using the ytstart command.")
 
