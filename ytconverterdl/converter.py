b10ff9a4fb40fe29117974e90e06ee22a575320e
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-11-23 13:40:30 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -1,123 +1,74 @@
 import discord
-from redbot.core import commands
-from yt_dlp import YoutubeDL
+from discord.ext import commands
 import aiohttp
-from aiohttp import web
+import yt_dlp
 import asyncio
 from redbot.core import data_manager
-from pathlib import Path
 
 class ConverterCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.data_folder = data_manager.cog_data_path(cog_instance=self)
-        self.app = web.Application()
-        self.app.router.add_route('GET', '/videos/{filename}', self.handle_video_request)
-        self.runner = web.AppRunner(self.app)
-        self.hostname = None  # Store the hostname for web server
-        self.port = 8080  # Default port
+        self.app = aiohttp.web.Application()
+        self.runner = aiohttp.web.AppRunner(self.app)
+        self.hostname = None
+        self.port = 8080
         asyncio.create_task(self.start_server())
 
     async def start_server(self):
-        await self.runner.setup()
-        site = web.TCPSite(self.runner, '0.0.0.0', self.port)
+        await self.bot.wait_until_ready()
+        site = aiohttp.web.TCPSite(self.runner, self.hostname, self.port)
         await site.start()
 
     async def handle_video_request(self, request):
-        filename = request.match_info['filename']
-        video_path = self.data_folder / filename
-        if video_path.exists():
-            with open(video_path, 'rb') as f:
-                response = web.StreamResponse()
-                response.content_type = 'video/mp4'  # Change the content type based on your file type
-                await response.prepare(request)
-                await response.write(f.read())
-                return response
-        else:
-            return web.Response(status=404)
+        filename = request.match_info.get('filename', '')
+        video_path = f"{self.data_folder}/{filename}"
+        if not video_path.endswith(('.mp3', '.mp4')) or not video_path.startswith(self.data_folder):
+            raise aiohttp.web.HTTPNotFound()
+        
+        with open(video_path, 'rb') as f:
+            return aiohttp.web.Response(body=f.read(), content_type='audio/mpeg' if video_path.endswith('.mp3') else 'video/mp4')
 
-    async def download_and_convert(self, ctx, url, to_mp3=False):
-        try:
-            output_folder = self.data_folder / ("mp3" if to_mp3 else "mp4")
+    async def download_and_convert(self, url, format):
+        ydl_opts = {
+            'format': 'bestaudio/best' if format == 'mp3' else 'bestvideo+bestaudio/best',
+            'outtmpl': f'{self.data_folder}/%(title)s.%(ext)s',
+        }
 
-            ydl_opts = {
-                'format': 'bestvideo+bestaudio/best' if not to_mp3 else 'bestaudio/best',
-                'outtmpl': str(output_folder / "%(id)s.{'mp3' if to_mp3 else 'webm'}"),
-            }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info_dict)
 
-            # Send "Converting..." message
-            conversion_message = await ctx.send(f"`Converting...`")
-
-            with YoutubeDL(ydl_opts) as ydl:
-                info_dict = ydl.extract_info(url, download=False)
-
-                if 'entries' in info_dict:
-                    video_id = info_dict['entries'][0]['id']
-                    ydl_opts['outtmpl'] = str(output_folder / f"{video_id}.{'mp3' if to_mp3 else 'webm'}")
-
-                    ydl.download([url])
-
-                    # Update message to indicate uploading
-                    await conversion_message.edit(content=f"`Uploading...`")
-                    file_path = output_folder / f"{video_id}.{'mp3' if to_mp3 else 'webm'}"
-
-                    # Serve the video using aiohttp
-                    download_link = f"http://{self.hostname}:{self.port}/videos/{video_id}"
-                    await ctx.send(f"{ctx.author.mention} `Done | Download Link: {download_link}`")
-
-                    # Remove the file after 1 minute if it exists
-                    await asyncio.sleep(60)
-                    if file_path.exists():
-                        file_path.unlink()
-                    return
-
-            raise ValueError("No suitable format available for download.")
-
-        except Exception as e:
-            error_message = str(e)
-            await ctx.send(f"{ctx.author.mention} `An error occurred during conversion. {error_message}`")
+        return filename
 
     @commands.command()
     async def ytmp3(self, ctx, url):
-        """
-        Converts a YouTube video to MP3.
-
-        Parameters:
-        `<url>` The URL of the video you want to convert.
-        """
-        await self.download_and_convert(ctx, url, to_mp3=True)
+        filename = await self.download_and_convert(url, 'mp3')
+        await ctx.send(f"Conversion complete! You can download the MP3 [here]({await self.get_download_url(filename)})")
 
     @commands.command()
     async def ytmp4(self, ctx, url):
-        """
-        Converts a YouTube video to MP4.
-
-        Parameters:
-        `<url>` The URL of the video you want to convert.
-        """
-        await self.download_and_convert(ctx, url, to_mp3=False)
+        filename = await self.download_and_convert(url, 'mp4')
+        await ctx.send(f"Conversion complete! You can download the MP4 [here]({await self.get_download_url(filename)})")
 
     @commands.command()
     @commands.is_owner()
-    async def ytcset(self, ctx, hostname: str, port: int = 8080):
-        """
-        Set the hostname and port for the web server.
-        """
+    async def ytcset(self, ctx, hostname, port):
         self.hostname = hostname
-        self.port = port
-        await ctx.send(f"Hostname set to {hostname}, Port set to {port}")
+        self.port = int(port)
+        await ctx.send(f"Web server settings updated. Hostname: {self.hostname}, Port: {self.port}")
 
     @commands.command()
-    async def ytcurl(self, ctx, video_id):
-        """
-        Get the download URL for a converted video.
-        """
-        download_link = f"http://{self.hostname}:{self.port}/videos/{video_id}"
-        await ctx.send(f"{ctx.author.mention} `Download Link: {download_link}`")
+    async def ytcurl(self, ctx, filename):
+        download_url = await self.get_download_url(filename)
+        await ctx.send(f"Download URL for {filename}: {download_url}")
 
     @commands.command()
     async def ytcsettings(self, ctx):
-        """
-        Get the current settings for the web server.
-        """
-        await ctx.send(f"Hostname: {self.hostname}, Port: {self.port}")
+        await ctx.send(f"Web server settings - Hostname: {self.hostname}, Port: {self.port}")
+
+    async def get_download_url(self, filename):
+        return f"http://{self.hostname}:{self.port}/videos/{filename}"
+
+def setup(bot):
+    bot.add_cog(ConverterCog(bot))
