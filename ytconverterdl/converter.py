db2de2ecfe89e3b462a5c9b850dbdbe66bf4dfab
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-12-02 11:16:59 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -1,120 +1,137 @@
-from redbot.core import commands, data_manager
 import discord
-import aiohttp
-import yt_dlp
+from discord.ext import commands, menus
+from yt_dlp import YoutubeDL
 import asyncio
+from redbot.core import data_manager
 from pathlib import Path
-import os
+
+class VideoQualityMenu(menus.Menu):
+    def __init__(self, qualities, original_message, ctx):
+        super().__init__(timeout=120, delete_message_after=True)
+        self.qualities = qualities
+        self.original_message = original_message
+        self.ctx = ctx
+
+    async def send_initial_message(self, ctx, channel):
+        return await channel.send(
+            f"Select the preferred video quality:",
+            view=self
+        )
+
+    async def format_page(self, menu, page):
+        return discord.Embed(
+            title="Video Quality Selection",
+            description="\n".join(f"{i}. {quality}" for i, quality in enumerate(self.qualities, start=1)),
+            color=discord.Color.blurple()
+        )
+
+    async def on_timeout(self):
+        await self.original_message.edit(content="Video quality selection timeout. Please run the command again.")
+
+    async def interaction_check(self, interaction):
+        if interaction.user != self.ctx.author:
+            await interaction.response.send_message("You cannot interact with this menu.", ephemeral=True)
+            return False
+        return True
+
+    @menus.button('\N{WHITE HEAVY CHECK MARK}')
+    async def on_select_button(self, payload):
+        await self.original_message.edit(content="Converting video...")
+
+        try:
+            selected_index = int(payload.component.label) - 1
+            selected_quality = self.qualities[selected_index]
+
+            await self.original_message.edit(content=f"Converting video to {selected_quality}...")
+
+            # Your conversion logic here
+            output_folder = Path.cwd() / "output"
+            output_folder.mkdir(parents=True, exist_ok=True)
+
+            ydl_opts = {
+                'format': selected_quality,
+                'outtmpl': str(output_folder / f"%(title)s.{'mp4'}"),
+            }
+
+            with YoutubeDL(ydl_opts) as ydl:
+                ydl.download([url])
+
+            user = self.ctx.message.author
+            downloaded_file_path = output_folder / f"{video_info['title']}.mp4"
+            renamed_file_path = output_folder / f"{video_info['title']}.mp4"
+
+            # Simulating successful conversion and upload
+            await asyncio.sleep(5)
+            await self.original_message.edit(content="Successful")
+            await self.ctx.send(f'{user.mention}, `Here is the converted video:`',
+                                file=discord.File(str(renamed_file_path)))
+        except Exception as e:
+            error_message = f"An error occurred during conversion. Error details: {str(e)}"
+            await self.ctx.send(f"`{error_message}`")
 
 class ConverterCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.data_folder = data_manager.cog_data_path(cog_instance=self)
-        self.app = aiohttp.web.Application()
-        self.runner = aiohttp.web.AppRunner(self.app)
-        self.hostname = "web.purplepanda.cc"
-        self.port = 4090
-        self.is_server_running = False
-        self.start_server_task = None
 
-    async def start_server(self):
-        await self.bot.wait_until_ready()
+    async def download_and_convert(self, ctx, url, to_mp3=False):
+        try:
+            output_folder = self.data_folder / ("mp3" if to_mp3 else "mp4")
 
-        if not self.is_server_running:
-            self.app.router.add_route('GET', '/videos/{filename}', self.handle_video_request)
-            site = aiohttp.web.TCPSite(self.runner, self.hostname, self.port)
+            ydl_opts = {
+                'format': 'bestaudio/best' if to_mp3 else 'bestvideo+bestaudio/best',
+                'outtmpl': str(output_folder / f"%(id)s.{'mp3' if to_mp3 else 'webm'}"),
+            }
 
-            await self.runner.setup()
-            await site.start()
+            conversion_message = await ctx.send(f"`Converting video...`")
 
-            self.is_server_running = True
+            with YoutubeDL(ydl_opts) as ydl:
+                info_dict = ydl.extract_info(url, download=False)
 
-    async def handle_video_request(self, request):
-        filename = request.match_info.get('filename', '')
-        video_path = Path(self.data_folder) / filename
-        if not video_path.is_file() or not video_path.parts[:len(Path(self.data_folder).parts)] == Path(self.data_folder).parts:
-            raise aiohttp.web.HTTPNotFound()
+                if 'entries' in info_dict:
+                    video_info = info_dict['entries'][0]
+                else:
+                    video_info = info_dict
 
-        with open(video_path, 'rb') as f:
-            return aiohttp.web.Response(body=f.read(), content_type='audio/mpeg' if video_path.suffix == '.mp3' else 'video/mp4')
+                ydl.download([url])
 
-    async def download_and_convert(self, ctx, url, format):
-        ydl_opts = {
-            'format': 'bestaudio/best' if format == 'mp3' else 'bestvideo+bestaudio/best',
-            'outtmpl': f'{self.data_folder}/%(id)s.%(ext)s',
-            'postprocessors': [{
-                'key': 'FFmpegVideoConvertor',
-                'preferedformat': 'mp4' if format == 'mp4' else 'webm'
-            }] if format == 'mp4' else []
-        }
+            await asyncio.sleep(5)
 
-        await ctx.send(f"Converting to {format}... This may take a moment.")
+            user = ctx.message.author
+            downloaded_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'webm'}"
+            renamed_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'mp4'}"
 
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(url, download=True)
-            filename = ydl.prepare_filename(info_dict)
+            downloaded_file_path.rename(renamed_file_path)
 
-        return filename
+            # Get available video qualities
+            qualities = [stream['format_note'] for stream in video_info['formats']]
+
+            if qualities:
+                # Display a dropdown menu for quality selection
+                view = VideoQualityMenu(qualities, conversion_message, ctx)
+                await view.start(ctx)
+            else:
+                await conversion_message.edit(content="No available video qualities.")
+        except Exception as e:
+            error_message = f"An error occurred during conversion. Error details: {str(e)}"
+            await ctx.send(f"`{error_message}`")
 
     @commands.command()
     async def ytmp3(self, ctx, url):
-        filename = await self.download_and_convert(ctx, url, 'mp3')
-        download_url = await self.get_download_url(filename)
-        await ctx.send(f"Conversion complete! You can download the MP3 [here]({download_url}) {ctx.author.mention}. This link will expire in 24 hours.")
+        """
+        Converts a YouTube video to MP3.
+
+        Parameters:
+        `<url>` The URL of the video you want to convert.
+        """
+        await self.download_and_convert(ctx, url, to_mp3=True)
 
     @commands.command()
     async def ytmp4(self, ctx, url):
-        filename = await self.download_and_convert(ctx, url, 'mp4')
-        download_url = await self.get_download_url(filename)
-        await ctx.send(f"Conversion complete! You can download the MP4 [here]({download_url}) {ctx.author.mention}. This link will expire in 24 hours.")
+        """
+        Converts a YouTube video to MP4.
 
-    @commands.command()
-    @commands.is_owner()
-    async def ytcset(self, ctx, hostname, port):
-        self.hostname = hostname
-        self.port = int(port)
-        await ctx.send(f"Web server settings updated. Hostname: {self.hostname}, Port: {self.port}")
-
-    @commands.command()
-    async def ytcurl(self, ctx, filename):
-        download_url = await self.get_download_url(filename)
-        await ctx.send(f"Download URL for {filename}: {download_url} {ctx.author.mention}. This link will expire in 24 hours.")
-
-    @commands.command()
-    async def ytcsettings(self, ctx):
-        await ctx.send(f"Web server settings - Hostname: {self.hostname}, Port: {self.port}")
-
-    @commands.command()
-    @commands.is_owner()
-    async def ytstart(self, ctx):
-        if not self.is_server_running:
-            self.start_server_task = asyncio.create_task(self.start_server())
-            await ctx.send("Web server started.")
-        else:
-            await ctx.send("Web server is already running.")
-
-    @commands.command()
-    @commands.is_owner()
-    async def ytstop(self, ctx):
-        if self.is_server_running:
-            await self.stop_server()
-            await ctx.send("Web server stopped.")
-        else:
-            await ctx.send("Web server is not running.")
-
-    async def get_download_url(self, filename):
-        if self.is_server_running:
-            base_url = f"http://{self.hostname}:{self.port}"
-            return f"{base_url}/videos/{filename}"
-        else:
-            return None
-
-    async def stop_server(self):
-        await self.runner.cleanup()
-        self.is_server_running = False
-
-    async def remove_file_after_24_hours(self, filename):
-        await asyncio.sleep(24 * 60 * 60)  # Sleep for 24 hours
-        filepath = Path(self.data_folder) / filename
-        if filepath.is_file():
-            os.remove(filepath)
+        Parameters:
+        `<url>` The URL of the video you want to convert.
+        """
+        await self.download_and_convert(ctx, url, to_mp3=False)
