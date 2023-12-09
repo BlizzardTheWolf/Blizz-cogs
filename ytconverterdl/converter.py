5fb4927ebcb85480f9bca524ea24f9b1c3c94c66
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-12-09 20:49:15 +0100

    Update converter.py

--- ytconverterdl/converter.py
+++ ytconverterdl/converter.py
@@ -60,41 +60,40 @@ class ConverterCog(commands.Cog):
                         ),
                         None,
                     )
-                    await interaction.response.send_message(f"Option selected: {selected_format.label}")  # Add this line
+
+                    if not selected_format:
+                        await interaction.response.send_message("`Invalid selection. Please try the command again.`")
+                        return
+
+                    # Use selected_format to get the selected format
+                    ydl_opts['format'] = selected_format.value if isinstance(selected_format, discord.SelectOption) else selected_format
+                    await interaction.response.send_message(f"`Converting video to {selected_format.label}...`")
+
+                    ydl.download([url])
+
+                    user = ctx.message.author
+                    downloaded_file_path = output_folder / f"{info['id']}.{'mp3' if to_mp3 else 'webm'}"
+                    renamed_file_path = output_folder / f"{info['id']}.{'mp3' if to_mp3 else 'mp4'}"
+                    
+                    # Try uploading the file
+                    try:
+                        await conversion_message.edit(content=f"`Uploading video...`")
+                        # Send a new message with the converted file, mentioning the user
+                        await ctx.send(f'{user.mention}, `Here is the converted video:`',
+                                       file=discord.File(str(renamed_file_path)))
+                        await conversion_message.edit(content=f"`Video uploaded successfully.`")
+                    except discord.errors.HTTPException as upload_error:
+                        # If uploading fails, send an error message
+                        await ctx.send(f"`An error occurred during upload. Please check the file and try again.\nError details: {upload_error}`")
+
+                    # Remove the file after 10 minutes if it exists
+                    if renamed_file_path.exists():
+                        renamed_file_path.unlink()
+
                 except asyncio.TimeoutError:
-                    await ctx.send("`Selection timeout. Please try the command again.`")
+                    await interaction.response.send_message("`Selection timeout. Please try the command again.`")
                     return
 
-                if not selected_format:
-                    await ctx.send("`Invalid selection. Please try the command again.`")
-                    return
-
-                # Use selected_format to get the selected format
-                ydl_opts['format'] = selected_format.value if isinstance(selected_format, discord.SelectOption) else selected_format
-                await message.edit(content=f"`Converting video to {selected_format}...`")
-
-                ydl.download([url])
-
-            await asyncio.sleep(5)
-
-            user = ctx.message.author
-            downloaded_file_path = output_folder / f"{info['id']}.{'mp3' if to_mp3 else 'webm'}"
-            renamed_file_path = output_folder / f"{info['id']}.{'mp3' if to_mp3 else 'mp4'}"
-            
-            # Try uploading the file
-            try:
-                await conversion_message.edit(content=f"`Uploading video...`")
-                # Send a new message with the converted file, mentioning the user
-                await ctx.send(f'{user.mention}, `Here is the converted video:`',
-                               file=discord.File(str(renamed_file_path)))
-                await conversion_message.edit(content=f"`Video uploaded successfully.`")
-            except discord.errors.HTTPException as upload_error:
-                # If uploading fails, send an error message
-                await ctx.send(f"`An error occurred during upload. Please check the file and try again.\nError details: {upload_error}`")
-
-            # Remove the file after 10 minutes if it exists
-            if renamed_file_path.exists():
-                renamed_file_path.unlink()
         except Exception as e:
             error_message = str(e)
             await ctx.send(f"`An error occurred during conversion. Please check the URL and try again.\nError details: {error_message}`")
