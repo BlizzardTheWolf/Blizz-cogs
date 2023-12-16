f4be75f7d5c75db5b0be9be0aa61072497e92dd5
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2023-12-16 12:29:38 +0100

    Update converter.py

--- ytconverter/converter.py
+++ ytconverter/converter.py
@@ -37,22 +37,27 @@ class ConverterCog(commands.Cog):
             downloaded_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'webm'}"
             renamed_file_path = output_folder / f"{video_info['id']}.{'mp3' if to_mp3 else 'mp4'}"
 
-            downloaded_file_path.rename(renamed_file_path)
+            # Check if the downloaded file exists before renaming
+            if downloaded_file_path.exists():
+                downloaded_file_path.rename(renamed_file_path)
 
-            # Try uploading the file
-            try:
-                await conversion_message.edit(content=f"`Uploading video...`")
-                # Send a new message with the converted file, mentioning the user
-                await ctx.send(f'{user.mention}, `Here is the converted video:`',
-                               file=discord.File(str(renamed_file_path)))
-                await conversion_message.edit(content=f"`Video uploaded successfully.`")
-            except discord.errors.HTTPException as upload_error:
-                # If uploading fails, send an error message
-                await ctx.send(f"`An error occurred during upload. Please check the file and try again.\nError details: {upload_error}`")
+                # Try uploading the file
+                try:
+                    await conversion_message.edit(content=f"`Uploading video...`")
+                    # Send a new message with the converted file, mentioning the user
+                    await ctx.send(f'{user.mention}, `Here is the converted video:`',
+                                   file=discord.File(str(renamed_file_path)))
+                    await conversion_message.edit(content=f"`Video uploaded successfully.`")
+                except discord.errors.HTTPException as upload_error:
+                    # If uploading fails, send an error message
+                    await ctx.send(f"`An error occurred during upload. Please check the file and try again.\nError details: {upload_error}`")
+            else:
+                await ctx.send("`An error occurred during conversion. The downloaded file does not exist.`")
 
             # Remove the file after 10 minutes if it exists
             if renamed_file_path.exists():
                 renamed_file_path.unlink()
+
         except Exception as e:
             error_message = str(e)
             await ctx.send(f"`An error occurred during conversion. Please check the URL and try again.\nError details: {error_message}`")
@@ -76,4 +81,3 @@ class ConverterCog(commands.Cog):
         `<url>` The URL of the video you want to convert.
         """
         await self.download_and_convert(ctx, url, to_mp3=False)
-
