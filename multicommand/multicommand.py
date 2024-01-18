906802258491aea839f5c51ac9f6f1aabcc36bb1
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2024-01-18 09:10:01 +0100

    Create multicommand.py

---
+++ multicommand/multicommand.py
@@ -0,0 +1,19 @@
+import discord
+from redbot.core import commands
+from copy import copy
+
+class MultiCommand(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_message(self, message):
+        if not message.content.startswith(">"):
+            return
+        prefix = (await self.bot.get_valid_prefixes(message.guild))[0]
+        raw = message.content[1:]
+        for com in raw.split("<>"):
+            com = com.strip()
+            new_message = copy(message)
+            new_message.content = prefix + com
+            await self.bot.process_commands(new_message)
