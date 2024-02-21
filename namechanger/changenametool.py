8ba6124493bf4753998e20df7852a6d747f6ed36
Blizzard the Wolf <60931830+BlizzardTheWolf@users.noreply.github.com>
2024-02-21 13:07:11 +0100

    Create changenametool.py

---
+++ namechanger/changenametool.py
@@ -0,0 +1,13 @@
+from redbot.core import commands
+
+class ChangeName(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def changename(self, ctx, *, new_name: str):
+        try:
+            await ctx.author.edit(nick=new_name)
+            await ctx.send(f"Your nickname has been changed to {new_name}.")
+        except commands.MissingPermissions:
+            await ctx.send("Sorry, you don't have permission to change your nickname.")
